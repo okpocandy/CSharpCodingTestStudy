975d40b706087d02d3fe56b8f2e3c2bf2edf67bd
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-07 20:39:17 +0900

    add 1152
    add 1152

---
+++ cSharp/문자열/1152.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _1152
+    {
+        public static void Main(string[] args)
+        {
+            int num;
+            string input = Console.ReadLine();
+            if (input.Equals(" "))
+                num = 0;
+            else
+            {
+                var inputSp = input.Trim().Split(' ');
+                num = inputSp.Length;
+            }
+            Console.WriteLine(num);
+        }
+    }
+}
