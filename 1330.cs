493fe55df63a58ada83dc39f579803b78eaa2ace
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-09 20:31:20 +0900

    add 1330

---
+++ 1330.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study
+{
+    internal class _1330
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            if (a > b)
+                Console.WriteLine(">");
+            else if (a < b)
+                Console.WriteLine("<");
+            else
+                Console.WriteLine("==");
+        }
+    }
+}
