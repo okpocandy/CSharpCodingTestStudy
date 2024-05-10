fc734a10a1279ace16383df6d2184a5a8481e02a
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-10 20:51:59 +0900

    add 2439

---
+++ cSharp/구현/2439.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study
+{
+    internal class _2439
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int inp = int.Parse(input);
+            string ans = null;
+            for (int i = 0; i < inp; i++)
+            {
+                ans += "*";
+                string tmp = ans.PadLeft(inp);
+                Console.WriteLine(tmp);
+                
+            }
+        }
+    }
+}
