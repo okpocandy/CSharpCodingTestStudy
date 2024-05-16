eb2afea01be1b6509982a3dff73dccc9ac9be615
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-16 11:17:17 +0900

    백준문제 2475 2741 추가
    백준문제 2475 2741 추가

---
+++ cSharp/구현/2475.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _2475
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Console.WriteLine((ints[0] * ints[0] + ints[1] * ints[1] + ints[2] * ints[2] + ints[3] * ints[3] + ints[4] * ints[4]) % 10);
+        }
+    }
+}
---
+++ cSharp/구현/2741.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _2741
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i <= input; i++) { sb.Append(i + "\n"); }
+            Console.WriteLine(sb);
+        }
+    }
+}
