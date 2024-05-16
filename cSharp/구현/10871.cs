27d2c450bb71e40a581dfeb5c87f030b7cb729f2
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-16 23:14:52 +0900

    백준문제 10871 추가
    백준문제 10871 추가

---
+++ cSharp/구현/10871.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _10871
+    {
+        static void Main(string[] args)
+        {
+            int[] NX = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            foreach (int num in numbers)
+            {
+                if (NX[1] > num)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+
+        }
+    }
+}
