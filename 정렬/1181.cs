c2cf61502d4afb4df3cb34f93459168153695766
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-17 18:25:08 +0900

    백준문제 1181 추가, 정렬 폴더 추가
    백준문제 1181 추가, 정렬 폴더 추가

---
+++ 정렬/1181.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace C_Sharp_Study.정렬
+{
+    internal class _1181
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            string[] strings = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                strings[i] = Console.ReadLine();
+            }
+            string[] answer = strings.Distinct().ToArray();
+            Array.Sort(answer, (a, b) =>
+            {
+                if (a.Length < b.Length)
+                    return -1;
+                else if (a.Length > b.Length)
+                    return 1;
+                else
+                    return string.Compare(a, b);
+            });
+
+            for (int i = 0; i < answer.Length; ++i)
+            {
+                Console.WriteLine(answer[i]);
+            }
+        }
+    }
+}
