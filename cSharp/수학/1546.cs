06961409b6c36c39d319321c4aff7bc74f5e2ff5
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-20 22:43:52 +0900

    백준문제 4153, 1181 추가
    백준문제 4153, 1181 추가

---
+++ cSharp/수학/1546.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _1546
+    {
+        static void Main(string[] args)
+        {
+            float N = int.Parse(Console.ReadLine());
+            float[] scores = Array.ConvertAll(Console.ReadLine().Split(), float.Parse);
+            float max_score = scores.Max();
+            float sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                scores[i] = scores[i] / max_score * 100;
+                sum += scores[i];
+            }
+            Console.WriteLine(sum / N);
+        }
+    }
+}
---
+++ cSharp/수학/4153.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _4153
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                if (ints[0] == 0 && ints[1] == 0 && ints[2] == 0)
+                    break;
+                int max_ints = ints.Max();
+                Array.Sort(ints);
+                if (ints[2] * ints[2] == ints[1] * ints[1] + ints[0] * ints[0])
+                    Console.WriteLine("right");
+                else
+                    Console.WriteLine("wrong");
+            }
+
+        }
+    }
+}
