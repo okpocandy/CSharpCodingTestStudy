4e2fbdc30e5ac35b3783b9bad31e96304dd3d9d2
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-20 18:23:35 +0900

    백준문제 1259, 1436 추가
    백준문제 1259, 1436 추가

---
+++ cSharp/문자열/1259.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _1259
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "0")
+                    break;
+                string rev_input = new string(input.Reverse().ToArray());
+
+                if (input.Equals(rev_input))
+                    Console.WriteLine("yes");
+                else
+                    Console.WriteLine("no");
+            }
+        }
+    }
+}
---
+++ cSharp/탐색/1436.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.탐색
+{
+    internal class _1436
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int ans = 0;
+            int i = 666;
+            while (true)
+            {
+                if (i.ToString().Contains("666"))
+                    ans++;
+                if (ans == num)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                i++;
+            }
+        }
+    }
+}
