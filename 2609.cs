450227e7161842580842b6b070fdfda6d3bff1c1
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-21 11:29:07 +0900

    백준문제 2609 추가
    백준문제 2609 추가

---
+++ 2609.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study
+{
+    internal class _2609
+    {
+        static void Main(string[] args)
+        {
+            int[] ab = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(ab);
+            int a = ab[0];
+            int b = ab[1];
+            int r = 987654321;
+            while (true)
+            {
+                r = b % a;
+                if (r == 0)
+                    break;
+                b = a;
+                a = r;
+            }
+            Console.WriteLine(a);
+            Console.WriteLine(ab[0] * ab[1] / a);
+        }
+        
+    }
+}
