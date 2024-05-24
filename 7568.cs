49d68d6a4de5247bf0a7af103b2fb87a5f1a7f38
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-24 19:32:32 +0900

    백준문제 7568 추가
    백준문제 7568 추가

---
+++ 7568.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study
+{
+    internal class _7568
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[][] arrays = new int[N][];
+            for(int i = 0; i < N; i++)
+            {
+                int[] xy = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                arrays[i] = xy;
+            }
+            int k = 0;
+            int[] rank = new int[N];
+
+            for(int i = 0; i < N; i++)
+            {
+                for (int j= 0; j < N; j++)
+                {
+                    if (arrays[i][0] < arrays[j][0] && arrays[i][1] < arrays[j][1])
+                    {
+                        k++;
+                    }
+                }
+                rank[i] = k+1;
+                k = 0;
+            }
+            for(int i = 0; i < N;i++)
+            {
+                Console.Write(rank[i]);
+                Console.Write(' ');
+            }
+            
+        }
+    }
+}
