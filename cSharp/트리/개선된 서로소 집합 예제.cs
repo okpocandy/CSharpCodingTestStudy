2257413ef4329e8b121d171623751609939036a5
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-06-04 18:21:24 +0900

    '이것이 코딩테스트다' 그래프 이론 문제 추가
    '이것이 코딩테스트다' 그래프 이론 문제 추가

---
+++ cSharp/트리/개선된 서로소 집합 예제.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study
+{
+    internal class 개선된_서로소_집합_예제
+    {
+        static void Main(string[] args)
+        {
+            int[] ve = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int v = ve[0];
+            int e = ve[1];
+            int[] parent = new int[v + 1];
+            for (int i = 1; i < v + 1; i++)
+                parent[i] = i;
+
+            for (int i = 0; i < e; i++)
+            {
+                int[] ab = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                UnionParent(parent, ab[0], ab[1]);
+            }
+
+            Console.WriteLine("각 우너소가 속한 집합: ");
+            for (int i = 1; i < v + 1; i++)
+                Console.WriteLine(FindParent(parent, i));
+
+            Console.WriteLine("부모 테이블: ");
+            for (int i = 1; i < v + 1; i++)
+                Console.WriteLine(parent[i]);
+        }
+
+        static int FindParent(int[] parent, int x)
+        {
+            if (parent[x] != x)
+                parent[x] =  FindParent(parent, parent[x]);
+            return parent[x];
+        }
+
+        static void UnionParent(int[] parent, int a, int b)
+        {
+            a = FindParent(parent, a);
+            b = FindParent(parent, b);
+            if (a < b)
+                parent[b] = a;
+            else
+                parent[a] = b;
+        }
+    }
+}
+
+/*
+ 
+6 4
+1 4
+2 3
+2 4
+5 6
+
+ */
---
+++ cSharp/트리/서로소 집합을 활용한 사이클 판별 예제.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study
+{
+    internal class 서로소_집합을_활용한_사이클_판별_예제
+    {
+        static void Main(string[] args)
+        {
+            int[] ve = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int v = ve[0];
+            int e = ve[1];
+            int[] parent = new int[v + 1];
+            for (int i = 1; i < v + 1; i++) { parent[i] = i; }
+            bool cycle = false;
+
+            for (int i = 0; i < e; i++)
+            {
+                int[] ab = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                if (FindParent(parent, ab[0]) == FindParent(parent, ab[1]))
+                {
+                    cycle = true;
+                    break;
+                }
+                else
+                    UnionParent(parent, ab[0], ab[1]);
+            }
+            if (cycle) { Console.WriteLine("사이클이 발새했습니다."); }
+            else { Console.WriteLine("사이클이 발생하지 않았습니다."); }
+           
+        }
+        static int FindParent(int[] parent, int x)
+        {
+            if (parent[x] != x)
+                parent[x] = FindParent(parent, parent[x]);
+            return parent[x];
+        }
+
+        static void UnionParent(int[] parent, int a, int b)
+        {
+            a = FindParent(parent, a);
+            b = FindParent(parent, b);
+            if (a < b)
+                parent[b] = a;
+            else
+                parent[a] = b;
+        }
+    }
+}
+
+/*
+
+3 3
+1 2
+1 3
+2 3
+
+*/
