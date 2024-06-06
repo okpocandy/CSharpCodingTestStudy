12700a9d2ca73a913f1cb21a07eba4029fcad845
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-06-06 09:31:09 +0900

    백준문제 4104 추가
    백준문제 4104 추가

---
+++ cSharp/구현/4101.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _4101
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                int[] ab = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                if (ab[0] == 0 && ab[1] == 0) { break; }
+                if (ab[0] > ab[1])
+                    Console.WriteLine("Yes");
+                else
+                    Console.WriteLine("No");
+            }
+            
+
+        }
+    }
+}
