e52726d809857b3bddfde5dc4e9584889af376fb
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-06-13 11:30:31 +0900

    백준 1929, 1654 추가
    백준 1929, 1654 추가

---
+++ cSharp/수학/1929.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _1929
+    {
+        static void Main(string[] args)
+        {
+            int[] mn = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int m = mn[0];
+            int n = mn[1];
+            StringBuilder sb = new StringBuilder();
+            List<int> ints = new List<int>();
+            for(int i=m; i<=n; i++) ints.Add(i);
+            for(int i=2; i<n+1; i++)
+            {
+                List<int> del = ints.FindAll(x => x%i==0);
+                del.Remove(i);
+                if(del.Count > 0)
+                {
+                    foreach (var x in del)
+                        ints.Remove(x);
+                }
+
+            }
+            foreach (var i in ints)
+                sb.Append(i + "\n");
+            Console.WriteLine(sb);
+        }
+    }
+}
+
+
+
+/*
+ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _1929
+    {
+        static void Main(string[] args)
+        {
+            int[] mn = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int m = mn[0];
+            int n = mn[1];
+
+            // 에라토스테네스의 체 알고리즘 사용
+            bool[] isPrime = new bool[n + 1];
+            for (int i = 2; i <= n; i++) isPrime[i] = true;
+
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (isPrime[i])
+                {
+                    for (int j = i * i; j <= n; j += i)
+                    {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+
+            // 소수 출력
+            StringBuilder sb = new StringBuilder();
+            for (int i = m; i <= n; i++)
+            {
+                if (isPrime[i])
+                {
+                    sb.Append(i).Append('\n');
+                }
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
+ */
---
+++ cSharp/탐색/1654.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.탐색
+{
+    internal class _1654
+    {
+        static int k;
+        static int n;
+        static List<int> res = new List<int>();
+        static void Main(string[] args)
+        {
+            int[] kn = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            k = kn[0];
+            n = kn[1];
+            int[] nums = new int[k];
+            for(int i = 0; i<k; i++)
+            {
+                int inp = int.Parse(Console.ReadLine());
+                nums[i] = inp;
+            }
+            long min_nums = nums.Max();
+            long ans = 0;
+            ans = BS(nums, 1, min_nums);
+            Console.WriteLine(ans);
+        }
+        
+        static long BS(int[] nums, long start, long end)
+        {
+            while (start<=end)
+            {
+                long mid = (start+end)/2;
+
+                long sum = 0;
+                for(int i=0; i<k;i++)
+                {
+                    sum += nums[i] / mid;
+                }
+                if (sum >= n)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            return end;
+        }
+        
+
+
+    }
+}
