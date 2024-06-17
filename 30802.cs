1a31382cf052c95e8b5d39ac51b2c65865816659
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-06-17 22:44:02 +0900

    백준문제 30802 추가
    백준문제 30802 추가

---
+++ 30802.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study
+{
+    internal class _30802
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] siz = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int[] tp = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int t = tp[0];
+            int p = tp[1];
+
+            int ansT = 0;
+            foreach (var i in siz)
+            {
+                if (i % t == 0)
+                    ansT += i / t;
+                else
+                    ansT += i / t + 1;
+            }
+            Console.WriteLine(ansT);
+            Console.WriteLine($"{n/p} {n%p}");
+        }
+    }
+}
