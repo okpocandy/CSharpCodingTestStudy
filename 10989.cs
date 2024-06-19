3a8127f758a55c2b6e72d539325cc85f74a25a73
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-06-19 11:27:27 +0900

    백준문제 10989, 2869 추가
    백준문제 10989, 2869 추가

---
+++ 10989.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace C_Sharp_Study
+{
+    internal class _10989
+    {
+        static void Main(string[] args)
+        {
+            int[] count = new int[10001]; // 각 수의 개수를 저장할 배열
+            StringBuilder sb = new StringBuilder();
+
+
+            int n = int.Parse(Console.ReadLine());
+
+            // 입력된 수의 개수를 카운트
+            for (int i = 0; i < n; i++)
+            {
+                count[int.Parse(Console.ReadLine())]++;
+            }
+
+            // 카운트 배열을 기반으로 정렬된 결과 출력
+            for (int i = 1; i < count.Length; i++)
+            {
+                if (count[i] != 0)
+                {
+                    for (int j = 0; j < count[i]; j++)
+                    {
+                        sb.Append(count[i]);
+                    }
+                }
+                if(i%100 == 0)
+                {
+                    Console.WriteLine(sb.ToString());
+                    sb.Clear();
+                }
+                    
+            }
+        }
+    }
+}
---
+++ cSharp/수학/2869.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _2869
+    {
+        static void Main(string[] args)
+        {
+            int[] abv = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int a = abv[0];
+            int b = abv[1];
+            int v = abv[2];
+            int tmp = a - b;
+            v -= a;
+            int ans = 1;
+            if (v <= 0) { Console.WriteLine(1); }
+            else
+            {
+                ans += v / tmp;
+                if (v % tmp != 0)
+                    ans++;
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
