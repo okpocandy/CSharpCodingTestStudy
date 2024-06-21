5316b6b965bb5d7ee525e791047e98010e02c961
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-06-21 13:05:37 +0900

    백준문제 1297 추가
    백준문제 1297 추가

---
+++ cSharp/수학/1297.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _1297
+    {
+        static void Main(string[] args)
+        {
+            int[] dhw = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int d = dhw[0];
+            int h = dhw[1];
+            int w = dhw[2];
+
+            double sD = d * d; ;
+            double sH = sD * (h * h) / (h * h + w * w);
+            double sW = sD * (w * w) / (h * h + w * w);
+            Console.WriteLine(Math.Floor(Math.Sqrt(sH)) + " " + Math.Floor(Math.Sqrt(sW)));
+        }
+    }
+}
