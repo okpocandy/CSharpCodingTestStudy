dd77804ca82e83204ce526a5154e8a1bc734dcde
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-06-26 20:50:28 +0900

    백준문제 17219, 1927 추가
    백준문제 17219, 1927 추가

---
+++ cSharp/자료구조/17219.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.자료구조
+{
+    internal class _17219
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nm[0];
+            int m = nm[1];
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                string[] inp = Console.ReadLine().Split();
+                dic[inp[0]] = inp[1];
+            }
+            for (int i = 0; i < m; i++)
+            {
+                string inp = Console.ReadLine();
+                sb.Append(dic[inp] + "\n");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ cSharp/자료구조/큐/1927.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.자료구조.큐
+{
+    internal class _1927
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if (a == 0)
+                {
+                    if (pq.Count == 0)
+                        sb.Append(0 + "\n");
+                    else
+                    {
+                        sb.Append(pq.Dequeue() + "\n");
+                    }
+                }
+                else
+                    pq.Enqueue(a, a);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
