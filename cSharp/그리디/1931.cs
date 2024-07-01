f5de4b9899d9cdd644f4716b0f23c252593c100a
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-07-01 18:05:44 +0900

    백준문제 1931 추가
    백준문제 1931 추가

---
+++ cSharp/그리디/1931.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study
+{
+    internal class _1931
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<(int, int)> ls = new List<(int, int)>();
+            for (int i=0; i<n; i++)
+            {
+                int[] inp = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                ls.Add((inp[0], inp[1]));
+            }
+            ls.Sort((a,b) =>
+            {
+                if(a.Item2 == b.Item2)
+                    return a.Item1.CompareTo(b.Item1);
+                else
+                    return a.Item2.CompareTo(b.Item2);
+            });
+            int st = 0;
+            int ed = 0;
+            int cnt = 0;
+            foreach(var i in ls)
+            {
+                if(i.Item1 >= ed)
+                {
+                    st = i.Item1;
+                    ed = i.Item2;
+                    cnt++;
+                }
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
