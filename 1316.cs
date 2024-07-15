9f7f231f10a296c8d33a61568f327341fc73eb7a
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-07-15 22:14:15 +0900

    백준문제 1316 추가
    백준문제 1316 추가

---
+++ 1316.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study
+{
+    internal class _1316
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            int cnt = 0;
+            for(int i=0; i<n; i++)
+            {
+                string a = Console.ReadLine();
+                List<char> ls = new List<char>();
+                char pre = ' ';
+                int f = 0;
+                
+                
+                foreach(var c in a)
+                {
+                    if(c == pre)
+                    {
+                        pre = c;
+                        continue;
+                    }
+                    else
+                    {
+                        if (ls.Contains(c))
+                        {
+                            f = 1;
+                            break;
+                        }
+                        else
+                        {
+                            ls.Add(c);
+                            pre = c;
+                        }
+                            
+
+                    }
+                }
+                if (f == 0)
+                    cnt++;
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
