c176fcb7fe081e6d6177dff9cac95d7d7a038f64
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-07-18 09:34:38 +0900

    백준문제 1120, 1065 추가
    백준문제 1120, 1065 추가

---
+++ cSharp/구현/1065.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _1065
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int cnt = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i < 100)
+                {
+                    cnt++;
+                    continue;
+                }
+                else if (i < 1000)
+                {
+                    int tmp = i;
+                    int a = i / 100;
+                    tmp -= a * 100;
+                    int b = tmp / 10;
+                    tmp -= b * 10;
+                    int c = tmp;
+                    if (a - b == b - c)
+                    {
+                        cnt++;
+                    }
+                }
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
---
+++ cSharp/문자열/1120.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _1120
+    {
+        static void Main(string[] args)
+        {
+            var inp = Console.ReadLine().Split();
+            string a = inp[0];
+            string b = inp[1];
+            int dif = b.Length - a.Length;
+            int min_cnt = 50;
+            for (int i = 0; i <= dif; i++)
+            {
+                int cnt = 0;
+                string tmp = b[i..(i + a.Length)];
+                for (int j = 0; j < tmp.Length; j++)
+                {
+                    if (a[j] != tmp[j])
+                    {
+                        cnt++;
+                    }
+                }
+                min_cnt = Math.Min(min_cnt, cnt);
+            }
+            Console.WriteLine(min_cnt);
+        }
+    }
+}
