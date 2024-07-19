dddaae1aecfa3be5e21b09e9d17c2540194e3630
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-07-19 19:13:49 +0900

    백준문제 1235, 1358 추가
    백준문제 1235, 1358 추가

---
+++ cSharp/문자열/1235.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _1235
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> ls = new List<string>();
+            for(int i=0; i<n; i++)
+            {
+                ls.Add(Console.ReadLine());  
+            }
+            int len = ls[0].Length;
+            int ans = len;
+            List<string> tmp = new List<string>();
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    var a = ls[j][(len - i-1)..];
+                    if (!tmp.Contains(a))
+                        tmp.Add(a);
+                    else
+                        break;
+                    if(j==n-1)
+                    {
+                        ans = i;
+                        break;
+                    }    
+                }
+                if (ans != len)
+                    break;
+                    
+            }
+            Console.WriteLine(ans+1);
+        }
+    }
+}
---
+++ cSharp/수학/1358.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study
+{
+    internal class _1358
+    {
+        static void Main(string[] args)
+        {
+            int[] inp = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int w = inp[0];
+            int h = inp[1];
+            int x = inp[2];
+            int y = inp[3];
+            int p = inp[4];
+            int cnt = 0;
+            for(int i=0; i<p; i++)
+            {
+                int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int cx = ints[0];
+                int cy = ints[1];
+                if(((cx - x) * (cx - x) + (cy - y - h/2) * (cy - y - h/2)) <= (h / 2) * (h / 2) || 
+                    (cx >= x && cy >= y && cx <= (x + w) && cy <= (y+h)) ||
+                    ((cx - x - w) * (cx - x - w) + (cy - y - h/2) * (cy - y - h/2)) <= (h / 2) * (h / 2))
+                {
+                    cnt++;
+                }
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
