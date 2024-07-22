472ef59cc712beb41365c200b4c3eca7871bb35b
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-07-22 11:13:48 +0900

    백준문제 2312 추가
    백준문제 2312 추가

---
+++ cSharp/수학/2312.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _2312
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int d = 2;
+                int cnt = 0;
+                while (n != 1)
+                {
+                    if (n % d == 0)
+                    {
+                        n = n / d;
+                        cnt++;
+                    }
+                    else
+                    {
+                        if (cnt != 0)
+                        {
+                            sb.Append(d + " " + cnt + "\n");
+
+                            cnt = 0;
+                        }
+                        d++;
+                    }
+                }
+                if (cnt != 0)
+                {
+                    sb.Append(d + " " + cnt + "\n");
+                    cnt = 0;
+                }
+
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
