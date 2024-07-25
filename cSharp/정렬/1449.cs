ebe2bba6906e290996fe749bc606eaf3b9badb04
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-07-25 10:17:13 +0900

    백준문제 1449 추가
    백준문제 1449 추가

---
+++ cSharp/정렬/1449.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _1449
+    {
+        static void Main(string[] args)
+        {
+            int[] nl = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nl[0];
+            int l = nl[1];
+
+            int[] ls = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(ls);
+            int index = 1;
+            int st = ls[0];
+            int cnt = 0;
+            while (true)
+            {
+                if (index >= ls.Length)
+                    break;
+                if (ls[index] <= st + l - 1)
+                {
+                    index++;
+                }
+                else
+                {
+                    cnt++;
+                    st = ls[index];
+                    index++;
+                }
+
+            }
+            Console.WriteLine(cnt + 1);
+        }
+    }
+}
