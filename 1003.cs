71efb343d6c24800dfbe6c929e028f64b7ac854b
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-07-30 20:16:55 +0900

    백준문제 1697, 1003 추가
    백준문제 1697, 1003 추가

---
+++ 1003.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study
+{
+    internal class _1003
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            int[,] ints = new int[41, 2];
+            ints[0, 0] = 1;
+            ints[0, 1] = 0;
+            ints[1, 0] = 0;
+            ints[1, 1] = 1;
+            for(int i=2; i<=40;  i++)
+            {
+                ints[i, 0] = ints[i - 1, 0] + ints[i - 2, 0];
+                ints[i,1] = ints[i - 1, 1] + ints[i - 2, 1];
+            }
+            for(int i=0; i<t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine(ints[n,0] + " " + ints[n,1]);
+            }
+        }
+
+    }
+}
---
+++ cSharp/탐색/BFS/1697.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.탐색.BFS
+{
+    internal class _1697
+    {
+        static void Main(string[] args)
+        {
+            int[] nk = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nk[0];
+            int k = nk[1];
+
+            int maxLength = Math.Max(n, k) * 2 + 1;
+            int[] visited = new int[maxLength];
+            BFS(visited, n, k);
+            Console.WriteLine(visited[k] - 1);
+        }
+
+        static void BFS(int[] visited, int start, int end)
+        {
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            visited[start] = 1;
+            while (q.Count > 0)
+            {
+                int n = q.Dequeue();
+                if (n == end)
+                {
+                    break;
+                }
+                if (n - 1 >= 0)
+                {
+                    if (visited[n - 1] == 0)
+                    {
+                        q.Enqueue(n - 1);
+                        visited[n - 1] = visited[n] + 1;
+                    }
+                }
+                if (n + 1 < 2 * end)
+                {
+                    if (visited[n + 1] == 0)
+                    {
+                        q.Enqueue(n + 1);
+                        visited[n + 1] = visited[n] + 1;
+                    }
+                }
+                if (2 * n < 2 * end)
+                {
+                    if (visited[2 * n] == 0)
+                    {
+                        q.Enqueue(2 * n);
+                        visited[2 * n] = visited[n] + 1;
+                    }
+                }
+            }
+        }
+    }
+}
