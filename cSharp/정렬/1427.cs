5941d230342362b131d202ec9c82feea4004fdad
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-08-01 13:34:27 +0900

    백준문제 1427 추가
    백준문제 1427 추가

---
+++ cSharp/정렬/1427.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _1427
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            var a = num.ToArray();
+            Array.Sort(a);
+            var rea = a.Reverse();
+            Console.WriteLine(string.Join("", rea));
+        }
+    }
+}
