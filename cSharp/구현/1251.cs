57f350cc18013917a5c57e0d1cd834104c7640a0
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-08-04 10:48:41 +0900

    백준문제 1251, 1476 추가
    백준문제 1251, 1476 추가

---
+++ cSharp/구현/1251.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _1251
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string ans = "";
+            for (int i = 0; i < s.Length; i++)
+                ans += "z";
+            for (int i = 1; i < s.Length - 1; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    string tmp1 = s[0..i];
+                    string tmp2 = s[i..j];
+                    string tmp3 = s[j..];
+                    string rtmp1 = new string(tmp1.Reverse().ToArray());
+                    string rtmp2 = new string(tmp2.Reverse().ToArray());
+                    string rtmp3 = new string(tmp3.Reverse().ToArray());
+                    string tmpsum = rtmp1 + rtmp2 + rtmp3;
+                    if (string.Compare(tmpsum, ans) <= 0)
+                    {
+                        ans = tmpsum;
+                    }
+                }
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ cSharp/수학/1476.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _1476
+    {
+        static void Main(string[] args)
+        {
+            int[] esm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int e = esm[0];
+            int s = esm[1];
+            int m = esm[2];
+            for(int i=1; i<987654321;i++)
+            {
+                if((i-e) % 15 == 0 && (i-s) % 28 == 0 && (i-m) % 19 ==0)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+
+            }
+        }
+    }
+}
