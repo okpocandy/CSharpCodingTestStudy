d0f494103e462f0eda63f57756db7d39a77ba5da
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-08-05 21:18:57 +0900

    백준문제 1940, 2003 추가
    백준문제 1940, 2003 추가

---
+++ cSharp/2003.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp
+{
+    internal class _2003
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nm[0];
+            int m = nm[1];
+            int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int left = 0;
+            int right = 1;
+            int cnt = 0;
+            while(left<n && right<=n)
+            {
+                int tmp = ints[left..right].Sum();
+                if (tmp < m)
+                    right++;
+                else if (tmp > m)
+                    left++;
+                else
+                {
+                    cnt++;
+                    left++;
+                    right++;
+                }
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
---
+++ cSharp/정렬/1940.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _1940
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(ints);
+            int cnt = 0;
+            int left = 0;
+            int right = n - 1;
+            while (left < right)
+            {
+                int tmp = ints[left] + ints[right];
+                if (tmp < m)
+                    left++;
+                else if (tmp > m)
+                    right--;
+                else
+                {
+                    cnt++;
+                    left++;
+                    right--;
+                }
+            }
+            Console.WriteLine(cnt);
+        }
+
+    }
+}
