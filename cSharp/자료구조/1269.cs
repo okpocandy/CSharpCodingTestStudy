80931bc7584098ffbf74f07971acc33a8994a947
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-08-07 22:11:19 +0900

    백준문제 1269, 1302 추가
    백준문제 1269, 1302 추가

---
+++ cSharp/자료구조/1269.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.자료구조
+{
+    internal class _1269
+    {
+        static void Main(string[] args)
+        {
+            int[] ab = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int a = ab[0];
+            int b = ab[1];
+            int[] ai = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int[] bi = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            HashSet<int> ah = new HashSet<int>();
+            HashSet<int> bh = new HashSet<int>();
+            HashSet<int> tmp = new HashSet<int>();
+            foreach (int i in ai)
+            {
+                ah.Add(i);
+                tmp.Add(i);
+            }
+            foreach (int i in bi)
+            {
+                bh.Add(i);
+            }
+            ah.IntersectWith(bh);
+            Console.WriteLine(tmp.Count + bh.Count - 2 * ah.Count);
+        }
+    }
+}
---
+++ cSharp/자료구조/1302.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.자료구조
+{
+    internal class _1302
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            for (int i = 0; i < n; i++)
+            {
+                string s = Console.ReadLine();
+                if (dic.ContainsKey(s))
+                    dic[s]++;
+                else
+                    dic[s] = 1;
+            }
+            var sortedDic = dic.OrderByDescending(entry => entry.Value)
+                               .ThenBy(entry => entry.Key);
+            Console.WriteLine(sortedDic.ToArray()[0].Key);
+
+        }
+    }
+}
