aa3a19df6a788619eff312d38e623e436507ec2c
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-08-11 13:11:27 +0900

    백준문제 1932, 5212 추가
    백준문제 1932, 5212 추가

---
+++ cSharp/DP/1932.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.DP
+{
+    internal class _1932
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int[]> ls = new List<int[]>();
+            for (int i = 0; i < n; i++)
+            {
+                ls.Add(Array.ConvertAll(Console.ReadLine().Split(), int.Parse)); ;
+
+            }
+            List<int[]> dp = new List<int[]>();
+            for (int i = 0; i < n; i++)
+            {
+                dp.Add(new int[i + 1]);
+            }
+            dp[0][0] = ls[0][0];
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0)
+                        dp[i][j] = dp[i - 1][j] + ls[i][j];
+                    else if (j == i)
+                        dp[i][j] = dp[i - 1][j - 1] + ls[i][j];
+                    else
+                        dp[i][j] = Math.Max(dp[i - 1][j - 1], dp[i - 1][j]) + ls[i][j];
+                }
+            }
+            Console.WriteLine(dp[n - 1].Max());
+        }
+    }
+}
---
+++ cSharp/구현/5212.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _5212
+    {
+        static void Main(string[] args)
+        {
+            int[] rc = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int r = rc[0];
+            int c = rc[1];
+            char[][] ls = new char[r][];
+            for (int i = 0; i < r; i++)
+            {
+                ls[i] = Console.ReadLine().ToCharArray();
+            }
+
+            // Padding the grid with a border of '.' characters
+            char[][] ls2 = new char[r + 2][];
+            string tmp = new string('.', c + 2);
+            ls2[0] = tmp.ToCharArray();
+            ls2[r + 1] = tmp.ToCharArray();
+            for (int i = 1; i < r + 1; i++)
+            {
+                ls2[i] = ("." + new string(ls[i - 1]) + ".").ToCharArray();
+            }
+
+            // Mark the cells that should change
+            char[][] result = new char[r + 2][];
+            for (int i = 0; i < r + 2; i++)
+            {
+                result[i] = (char[])ls2[i].Clone();
+            }
+
+            for (int i = 1; i < r + 1; i++)
+            {
+                for (int j = 1; j < c + 1; j++)
+                {
+                    if (ls2[i][j] == 'X')
+                    {
+                        int cnt = 0;
+                        if (ls2[i - 1][j] == '.') cnt++;
+                        if (ls2[i][j - 1] == '.') cnt++;
+                        if (ls2[i + 1][j] == '.') cnt++;
+                        if (ls2[i][j + 1] == '.') cnt++;
+
+                        if (cnt >= 3)
+                        {
+                            result[i][j] = '.'; // Mark for change
+                        }
+                    }
+                }
+            }
+            int minX = r + 1, maxX = 0, minY = c + 1, maxY = 0;
+            for (int i = 1; i < r + 1; i++)
+            {
+                for (int j = 1; j < c + 1; j++)
+                {
+                    if (result[i][j] == 'X')
+                    {
+                        minX = Math.Min(minX, i);
+                        maxX = Math.Max(maxX, i);
+                        minY = Math.Min(minY, j);
+                        maxY = Math.Max(maxY, j);
+                    }
+                }
+            }
+
+            for (int i = minX; i <= maxX; i++)
+            {
+                for (int j = minY; j <= maxY; j++)
+                {
+                    Console.Write(result[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
