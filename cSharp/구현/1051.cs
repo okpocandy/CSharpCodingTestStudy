ba0a6d91767942e69eb16ed5deb5a15d1e3d1306
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-08-12 20:00:50 +0900

    백준문제 1051, 1735 추가
    백준문제 1051, 1735 추가

---
+++ cSharp/구현/1051.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _1051
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nm[0];
+            int m = nm[1];
+            string[] ls = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                ls[i] = Console.ReadLine();
+
+            }
+            int mi = Math.Min(n, m);
+            int ans = 0;
+            for (int i = 1; i < mi; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                {
+                    for (int k = 0; k < m - i; k++)
+                    {
+                        if (ls[j][k] == ls[j + i][k] && ls[j][k] == ls[j][k + i] && ls[j][k] == ls[j + i][k + i])
+                            ans = i;
+                    }
+                }
+            }
+            Console.WriteLine((ans + 1) * (ans + 1));
+        }
+    }
+}
---
+++ cSharp/수학/1735.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _1735
+    {
+        static void Main(string[] args)
+        {
+            int[] aab = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int at = aab[0];
+            int ab = aab[1];
+            int[] bab = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int bt = bab[0];
+            int bb = bab[1];
+            int b = ab * bb / gcd(ab, bb);
+            int t = at * (b / ab) + bt * (b / bb);
+
+            int tmp = Math.Min(t, b);
+            int index = 2;
+            while (index <= tmp)
+            {
+                while (b % index == 0 && t % index == 0)
+                {
+                    b = b / index;
+                    t = t / index;
+                }
+                index++;
+            }
+            Console.WriteLine(t + " " + b);
+        }
+        static int gcd(int n, int m)
+        {
+            if (m == 0) return n;
+            else
+                return gcd(m, n % m);
+        }
+    }
+}
