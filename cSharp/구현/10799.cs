2f46d0e816e9553deb4756a5a48424594a9f2fab
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-08-13 14:01:43 +0900

    백준문제 10799, 11725 추가
    백준문제 10799, 11725 추가

---
+++ cSharp/구현/10799.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp
+{
+    internal class _10799
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Stack<char> st = new Stack<char>();
+            int ans = 0;
+            int cnt = 0;
+            char pre = '(';
+            foreach(var i in s)
+            {
+                if(i == '(')
+                {
+                    st.Push(i);
+                    cnt++;
+                    pre = '(';
+                }
+                    
+                else
+                {
+                    if(pre == '(')
+                    {
+                        st.Pop();
+                        cnt--;
+                        ans += cnt;
+                    }
+                    else
+                    {
+                        cnt--;
+                        ans++;
+                    }
+                    pre = ')';
+                }
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ cSharp/탐색/BFS/11725.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.탐색.BFS
+{
+    internal class _11725
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] parent = new int[n + 1];
+            StringBuilder sb = new StringBuilder();
+            List<int>[] graph = new List<int>[n + 1];
+            for (int i = 1; i <= n; i++)
+            {
+                graph[i] = new List<int>();
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                int[] uv = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int u = uv[0];
+                int v = uv[1];
+                graph[u].Add(v);
+                graph[v].Add(u);
+
+
+            }
+            BFS(graph, 1, parent);
+            for (int i = 2; i <= n; i++)
+            {
+                sb.Append(parent[i] + "\n");
+            }
+            Console.WriteLine(sb.ToString());
+
+
+        }
+        static void BFS(List<int>[] graph, int start, int[] parent)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(start);
+            while (queue.Count > 0)
+            {
+                int node = queue.Dequeue();
+                foreach (int i in graph[node])
+                {
+                    if (parent[i] == 0)
+                    {
+                        queue.Enqueue(i);
+                        parent[i] = node;
+                    }
+                }
+            }
+        }
+
+    }
+
+
+}
