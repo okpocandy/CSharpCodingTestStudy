04fc87682e2103549b3f055dbe1aa4b10c87b723
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-08-15 17:20:27 +0900

    백준문제 2941, 10815 추가
    백준문제 2941, 10815 추가

---
+++ cSharp/구현/2941.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _2941
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int cnt = 0;
+            char pre = s[0];
+            char ppre = s[0];
+            foreach (var i in s)
+            {
+                switch (i)
+                {
+                    case 'j':
+                        {
+                            if (pre != 'l' && pre != 'n')
+                                cnt++;
+                        }
+                        break;
+                    case '-':
+                        break;
+                    case '=':
+                        {
+                            if (ppre == 'd' && pre == 'z')
+                            {
+                                cnt--;
+                            }
+                        }
+                        break;
+                    default:
+                        cnt++;
+                        break;
+                }
+                ppre = pre;
+                pre = i;
+
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
---
+++ cSharp/자료구조/10815.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.자료구조
+{
+    internal class _10815
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<int> numbers = new HashSet<int>();
+            int[] ls = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            foreach(var i in ls)
+                numbers.Add(i);
+            int m = int.Parse(Console.ReadLine());
+            int[] fd = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int[] ans = new int[m];
+            for(int i = 0; i < m; i++)
+            {
+                if (numbers.Contains(fd[i]))
+                    ans[i] = 1;
+            }
+            Console.WriteLine(string.Join (" ", ans));
+        }
+    }
+}
