c5fe47433f7a98ad9eb39cbfced279829cbce7ed
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-08-26 22:13:08 +0900

    백준문제 1110, 4344 추가
    백준문제 1110, 4344 추가

---
+++ cSharp/구현/1110.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _1110
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int original = n;
+            int cnt = 0;
+
+            while (true)
+            {
+                int a = n / 10; // 십의 자리
+                int b = n % 10; // 일의 자리
+                int sum = (a + b) % 10; // 합의 일의 자리
+                n = b * 10 + sum; // 새로운 수 생성
+                cnt++;
+
+                if (n == original)
+                    break;
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
---
+++ cSharp/수학/4344.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _4344
+    {
+        static void Main(string[] args)
+        {
+            int c = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < c; i++)
+            {
+                int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int n = ints[0];
+                int[] arr = ints[1..];
+                double avr = (double)arr.Sum() / n;
+                double cnt = arr.Count(x => x > avr);
+                double ans = cnt / n * 100;
+                sb.Append(Math.Round(ans, 3).ToString("F3") + '%' + "\n");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
