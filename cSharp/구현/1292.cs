adac827e48e3f0f9aa8adba869b0622e6ba14d84
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-08-28 03:26:03 +0900

    백준문제 1292, 9093 추가
    백준문제 1292, 9093 추가

---
+++ cSharp/구현/1292.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _1292
+    {
+        static void Main(string[] args)
+        {
+            int[] ab = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int a = ab[0];
+            int b = ab[1];
+            int[] ints = new int[1001];
+            int cnt = 0;
+            for (int i = 1; i <= b; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    ints[cnt++] = i;
+                    if (cnt == b)
+                        break;
+                }
+                if (cnt == b)
+                    break;
+            }
+            Console.WriteLine(ints[(a - 1)..b].Sum());
+        }
+    }
+}
---
+++ cSharp/문자열/9093.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _9093
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i=0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+                string[] tmp = new string[s.Length];
+                for(int j=0; j<s.Length; j++)
+                {
+                    tmp[j] = new string ( s[j].Reverse().ToArray() );
+                }
+                Console.WriteLine(string.Join(" ", tmp));
+            }
+        }
+    }
+}
