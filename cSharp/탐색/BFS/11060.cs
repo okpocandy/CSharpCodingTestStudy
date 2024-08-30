3b9caa3c3b06e2c634f02a799c20f82fc29083e0
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-08-30 20:20:52 +0900

    백준문제 11060, 16953 추가
    백준문제 11060, 16953 추가

---
+++ cSharp/탐색/BFS/11060.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_Sharp_Study.cSharp.탐색.BFS
+{
+    internal class _11060
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int[] visited = new int[n];
+            Array.Fill(visited, -1); // 방문 안한 곳은 -1로 초기화
+            BFS(ints, n, visited);
+            Console.WriteLine(visited[n - 1]);
+        }
+
+        static void BFS(int[] ints, int n, int[] visited)
+        {
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(0);
+            visited[0] = 0; // 시작 위치는 0번 점프
+
+            while (q.Count > 0)
+            {
+                int loc = q.Dequeue();
+                int ma = ints[loc];
+
+                for (int i = 1; i <= ma; i++)
+                {
+                    int nextLoc = loc + i;
+                    if (nextLoc >= n) break; // 배열의 끝을 넘는 경우는 무시
+
+                    if (visited[nextLoc] == -1 || visited[nextLoc] > visited[loc] + 1)
+                    {
+                        visited[nextLoc] = visited[loc] + 1;
+                        q.Enqueue(nextLoc);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ cSharp/탐색/BFS/16953.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.탐색.BFS
+{
+    internal class _16953
+    {
+        static void Main(string[] args)
+        {
+            long[] ab = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+            long a = ab[0];
+            long b = ab[1];
+
+            int ans = BFS(a, b);
+            if (ans == 0)
+                Console.WriteLine("-1");
+            else
+                Console.WriteLine(ans+1);
+
+        }
+
+        static int BFS(long a, long b)
+        {
+            Queue<(long, int)> q = new Queue<(long, int)>();
+            q.Enqueue((a, 0));
+            
+            while (q.Count > 0)
+            {
+                (long n, int cnt)= q.Dequeue();
+                if(n == b)
+                {
+                    return cnt;
+                }
+                long nm = n * 2;
+                long no = n * 10 + 1;
+                if(nm <= b)
+                    q.Enqueue((nm, cnt+1));
+                if(no <= b)
+                    q.Enqueue((no, cnt+1));
+
+            }
+            return 0;
+        }
+       
+    }
+}
