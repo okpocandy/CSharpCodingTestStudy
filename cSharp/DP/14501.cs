6ec43397278372b83a6f5fdf30d7d6c5d924abc2
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-08-31 19:59:53 +0900

    백준문제 14501, 9655 추가
    백준문제 14501, 9655 추가

---
+++ cSharp/DP/14501.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.DP
+{
+    internal class _14501
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int[]> shedule = new List<int[]>();
+            int[] dp = new int[n+1];
+            for(int i=0; i<n; i++)
+            {
+                shedule.Add(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+
+            }
+            for(int i=0; i<n; i++)
+            {
+                if (dp[i] > dp[i + 1])
+                {
+                    dp[i + 1] = dp[i];
+                }
+                if (shedule[i][0] + i <= (n))
+                {
+                    dp[shedule[i][0] + i] = Math.Max(shedule[i][1] + dp[i], dp[shedule[i][0] + i]);
+                }
+            }
+            foreach(var i in dp)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(dp.Max());
+
+        }
+    }
+}
---
+++ cSharp/구현/9655.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _9655
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+                Console.WriteLine("CY");
+            else
+                Console.WriteLine("SK");
+        }
+    }
+}
