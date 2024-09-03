168630391062b59897c8b13b38636b013e64cf09
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-09-03 18:44:19 +0900

    백준문제 1158, 1193 추가
    백준문제 1158, 1193 추가

---
+++ cSharp/구현/1158.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _1158
+    {
+        static void Main(string[] args)
+        {
+            int[] nk = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nk[0];
+            int k = nk[1];
+            List<int> ans = new List<int>();
+            Queue<int> q = new Queue<int>();
+            for(int i=1; i<=n; i++)
+            {
+                q.Enqueue(i);
+            }
+            while(q.Count > 0)
+            {
+                for(int i=0; i<k-1; i++)
+                {
+                    int d = q.Dequeue();
+                    q.Enqueue(d);
+                }
+                ans.Add(q.Dequeue());
+            }
+            Console.WriteLine("<" + string.Join(", ", ans.ToArray())+">");
+        }
+    }
+}
---
+++ cSharp/구현/1193.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _1193
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int index = 1;
+            int sum = 2;
+            while(index < n)
+            {
+                index += sum;
+                sum++;
+            }
+            index -= (sum - 1);
+            n -= index;
+            if(sum%2 == 0)
+            {
+                Console.WriteLine((sum - n) + "/" + n);
+            }
+            else
+            {
+
+                Console.WriteLine(n + "/" + (sum - n));
+            }
+        }
+    }
+}
