77f5f3f9af1513a8bf410b491f80d6219bd06ebb
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-09-04 21:56:05 +0900

    백준문제 14215 추가
    백준문제 14215 추가

---
+++ cSharp/구현/14215.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _14215
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(ints);
+            int sum = ints[0] + ints[1];
+            if (ints[2] < sum)
+            {
+                Console.WriteLine(sum + ints[2]);
+            }
+            else
+            {
+                Console.WriteLine(sum + sum-1);
+            }
+        }
+    }
+}
