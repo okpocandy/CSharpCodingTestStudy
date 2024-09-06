48ba60a739fc9557d286737a31cf61cd54477df1
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-09-06 19:12:11 +0900

    백준문제 10988 추가
    백준문제 10988 추가

---
+++ cSharp/문자열/10988.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    class _10988
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string a = new string(s.Reverse().ToArray());
+            if (a == s)
+            {
+                Console.WriteLine(1);
+            }
+            else
+                Console.WriteLine(0);
+        }
+    }
+}
