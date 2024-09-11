4eca1708c4f32de8e362abff8dab1c06c19a4634
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-09-12 00:54:30 +0900

    백준문제 18258 추가
    백준문제 18258 추가

---
+++ cSharp/자료구조/큐/18258.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.자료구조.큐
+{
+    internal class _18258
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<int> q = new Queue<int>();
+            List<int> ans = new List<int>();
+            for(int i=1; i<=n; i++)
+            {
+                q.Enqueue(i);
+            }
+            while(true)
+            {
+                ans.Add(q.Dequeue());
+                if (q.Count == 0)
+                    break;
+                int tmp = q.Dequeue();
+                q.Enqueue(tmp);
+            }
+            Console.WriteLine(string.Join(" ", ans));
+        }
+    }
+}
