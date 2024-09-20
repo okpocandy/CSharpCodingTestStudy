00de5ce99c6850203d18b83bc6394d4643f47499
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-09-20 14:56:20 +0900

    백준문제 13909 추가
    백준문제 13909 추가

---
+++ cSharp/수학/13909.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _13909
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int cnt = 0;
+            for(int i=1; i*i<=n; i++)
+            {
+                cnt++;
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
