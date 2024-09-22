b39ccbbedcee154af2ff69ca836d15b93c326959
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-09-22 20:06:37 +0900

    백준문제 13417, 9536 추가
    백준문제 13417, 9536 추가

---
+++ cSharp/문자열/13417.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _13417
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for(int i=0; i< t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                LinkedList<string> ls = new LinkedList<string>();
+                string[] chars = Console.ReadLine().Split();
+                ls.AddLast(chars[0]);
+                string mi = chars[0];
+                for(int j=1; j< chars.Length; j++)
+                {
+                    if (chars[j].CompareTo(mi) <= 0)
+                    {
+                        ls.AddFirst(chars[j]);
+                        mi = chars[j];
+                    }
+                    else
+                    {
+                        ls.AddLast(chars[j]);
+                    }
+                }
+                Console.WriteLine(string.Join("", ls));
+            }
+        }
+    }
+}
---
+++ cSharp/문자열/9536.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    class _9536
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                StringBuilder sb = new StringBuilder();
+                string[] cries = Console.ReadLine().Split();
+                while(true)
+                {
+                    string[] tmp = Console.ReadLine().Split();
+                    if (tmp[^1].Equals("say?"))
+                        break;
+                    else
+                    {
+                        cries = cries.Where(a => a.Equals(tmp[^1]) == false).ToArray();
+                    }
+                }
+                sb.Append(string.Join(" ", cries));
+                Console.WriteLine(sb.ToString());
+                sb.Clear();
+            }
+
+            
+        }
+    }
+}
