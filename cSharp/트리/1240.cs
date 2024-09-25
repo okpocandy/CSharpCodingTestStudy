2d9134e9725c09123da8864fb29bf1688101acf9
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-09-25 17:29:52 +0900

    백준문제 1240, 17073 추가
    백준문제 1240, 17073 추가

---
+++ cSharp/트리/1240.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.트리
+{
+    internal class _1240
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nm[0];
+            int m = nm[1];
+            List<(int, int)>[] graph = new List<(int, int)>[n + 1];
+            
+            for (int i = 0; i < n + 1; i++)
+            {
+                graph[i] = new List<(int, int)>();
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                graph[ints[0]].Add((ints[1], ints[2]));
+                graph[ints[1]].Add((ints[0], ints[2]));
+            }
+            for(int i=0; i<m; i++)
+            {
+                int[] se = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int[] visited = new int[n + 1];
+                BFS(se[0], se[1], visited, graph);
+                Console.WriteLine(visited[se[1]]);
+            }
+        }
+
+        static void BFS(int start, int end, int[] visited, List<(int, int)>[] graph)
+        {
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            visited[start] = 0;
+            while (q.Count > 0)
+            {
+                int node = q.Dequeue();
+                foreach (var i in graph[node])
+                {
+                    if (visited[i.Item1] == 0)
+                    {
+                        visited[i.Item1] = visited[node] + i.Item2;
+                        q.Enqueue(i.Item1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ cSharp/트리/17073.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.트리
+{
+    internal class _17073
+    {
+        static void Main(string[] args)
+        {
+            int[] nw = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nw[0];
+            int w = nw[1];
+            List<int>[] graph = new List<int>[n+1];
+            int cnt = 0;
+            for(int i=0; i<n+1; i++)
+            {
+                graph[i] = new List<int>();
+            }
+            for(int i=0; i<n-1; i++)
+            {
+                int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                graph[ints[0]].Add(ints[1]);
+                graph[ints[1]].Add(ints[0]);
+            }
+            for(int i=2; i<=n; i++)
+            {
+                if (graph[i].Count == 1)
+                    cnt++;
+            }
+            double ans = (double)w / (double)cnt;
+            Console.WriteLine(ans);
+        }
+    }
+}
