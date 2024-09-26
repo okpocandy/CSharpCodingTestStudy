29562f613f8760de99ccec0c635ac0fdb9a26281
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-09-27 01:38:55 +0900

    백준문제 11656, 25757 추가
    백준문제 11656, 25757 추가

---
+++ cSharp/문자열/11656.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _11656
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            List<string> strings = new List<string>();
+            for (int i=0; i<s.Length; i++)
+            {
+                strings.Add(s[i..]);
+            }
+            strings.Sort();
+            StringBuilder sb = new StringBuilder();
+            foreach(var i in strings)
+            {
+                sb.AppendLine(i);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ cSharp/문자열/25757.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    class _25757
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine().Split();
+            int n = int.Parse(strings[0]);
+            string g = strings[1];
+            HashSet<string> name = new HashSet<string>();
+            for (int i = 0; i < n;i++)
+            {
+                string s = Console.ReadLine();
+                name.Add(s);
+            }
+            if(g == "Y")
+            {
+                Console.WriteLine(name.Count);
+            }
+            else if(g == "F")
+                Console.WriteLine((name.Count)/2);
+            else
+                Console.WriteLine((name.Count) / 3);
+        }
+    }
+}
