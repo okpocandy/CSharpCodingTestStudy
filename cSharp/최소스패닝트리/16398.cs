3ec83c22ba1a2f3fa5140057ccc33e3945c676e3
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-09-30 19:03:46 +0900

    백준문제 16398, 21924 추가
    백준문제 16398, 21924 추가

---
+++ cSharp/최소스패닝트리/16398.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.최소스패닝트리
+{
+    internal class _16398
+    {
+        class Edge : IComparable<Edge>
+        {
+            public int Src, Dest, Weight;
+
+            public Edge(int src, int dest, int weight)
+            {
+                Src = src;
+                Dest = dest;
+                Weight = weight;
+            }
+
+            public int CompareTo(Edge other)
+            {
+                return this.Weight.CompareTo(other.Weight);
+            }
+        }
+
+        class DisjoinSet
+        {
+            private int[] parent, rank;
+
+            public DisjoinSet(int n)
+            {
+                parent = new int[n];
+                rank = new int[n];
+                for (int i = 0; i < n; i++) parent[i] = i;
+            }
+
+            public int Find(int i)
+            {
+                if (parent[i] != i)
+                    parent[i] = Find(parent[i]);
+                return parent[i];
+            }
+
+            public void Union(int x, int y)
+            {
+                int rootX = Find(x);
+                int rootY = Find(y);
+
+                if (rootX != rootY)
+                {
+                    if (rank[rootX] < rank[rootY])
+                        parent[rootX] = rootY;
+                    else if (rank[rootX] > rank[rootY])
+                        parent[rootY] = rootX;
+                    else
+                    {
+                        parent[rootY] = rootX;
+                        rank[rootX]++;
+                    }
+                }
+            }
+
+            class KruskalMST
+            {
+                public List<Edge> Kruskal(List<Edge> edges, int V)
+                {
+                    List<Edge> result = new List<Edge>();
+                    edges.Sort();
+
+                    DisjoinSet ds = new DisjoinSet(V + 1);
+
+                    foreach (var edge in edges)
+                    {
+                        int x = ds.Find(edge.Src);
+                        int y = ds.Find(edge.Dest);
+
+                        if (x != y)
+                        {
+                            result.Add(edge);
+                            ds.Union(x, y);
+                        }
+
+                        if (result.Count == V - 1)
+                            break;
+                    }
+                    return result;
+                }
+
+            }
+
+            static void Main(string[] args)
+            {
+                int n = int.Parse(Console.ReadLine());
+                List<Edge> edges = new List<Edge>();
+                for(int i=0; i<n; i++)
+                {
+                    int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                    for(int j=i+1; j<n; j++)
+                    {
+                        if (ints[j] > 0)
+                            edges.Add(new Edge(i, j, ints[j]));
+
+                    }
+                }
+                edges.Sort();
+                KruskalMST mst = new KruskalMST();
+                List<Edge> result = mst.Kruskal(edges, n);
+                long cnt = 0;
+                foreach (var edge in result)
+                {
+                    cnt += edge.Weight;
+                }
+                Console.WriteLine(cnt);
+
+
+            }
+        }
+    }
+}
