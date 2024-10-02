2648bc378247f2c955270c3b484d1d0e9532af08
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-02 23:16:51 +0900

    백준문제 10798, 2745 추가
    백준문제 10798, 2745 추가

---
+++ cSharp/문자열/10798.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _10798
+    {
+        static void Main(string[] args)
+        {
+            List<char[]> ls = new List<char[]>();
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<5; i++)
+            {
+                ls.Add(Console.ReadLine().ToArray());
+            }
+            for(int i=0; i<15; i++)
+            {
+                for(int j=0; j<5; j++)
+                {
+                    if (ls[j].Length > i)
+                        sb.Append(ls[j][i]);
+                }
+            }
+            Console.WriteLine(sb.ToString());
+
+        }
+    }
+}
---
+++ cSharp/문자열/2745.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _2745
+    {
+        static void Main(
+            string[] args)
+        {
+            string[] tmp = Console.ReadLine().Split();
+            int b = int.Parse(tmp[1]);
+            List<int> ls = new List<int>();
+            foreach(var i in tmp[0])
+            {
+                if ((int)i >= (int)'A')
+                    ls.Add((int)i - (int)'7');
+                else
+                    ls.Add((int)i - 48);
+
+            }
+            double ans = 0;
+            int index = ls.Count-1;
+            foreach (var i in ls)
+            {
+                ans += i * Math.Pow(b, index--);
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
