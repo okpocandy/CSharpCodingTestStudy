85c1cb7c5febb8b99a10d387242e56f7ad5e2725
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-11 01:49:06 +0900

    백준문제 11478, 1254 추가
    백준문제 11478, 1254 추가

---
+++ cSharp/문자열/11478.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _11478
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            HashSet<string> set = new HashSet<string>();
+            for(int i=1; i<=s.Length; i++)
+            {
+                for(int j=0; j<=s.Length-i; j++)
+                {
+                    set.Add(s[j..(j+i)]);
+                }
+            }
+            Console.WriteLine(set.Count);
+        }
+    }
+}
---
+++ cSharp/문자열/1254.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _1254
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int ans = 0;
+            for(int i=0; i<s.Length; i++)
+            {
+                string subStr = s[i..];  // 부분 문자열
+                string reversedSubStr = new string(subStr.Reverse().ToArray());  // 뒤집기
+                if (subStr == reversedSubStr)
+                {
+                    ans = s.Length - i;
+                    break;
+                } 
+            }
+            Console.WriteLine((s.Length-ans) * 2 + ans);
+        }
+    }
+}
