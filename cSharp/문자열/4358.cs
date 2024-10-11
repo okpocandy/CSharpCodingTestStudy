d50161532fb395796234de50025374e3ab6d4bb5
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-11 21:50:58 +0900

    백준문제 4358 추가
    백준문제 4358 추가

---
+++ cSharp/문자열/4358.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _4358
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> trees = new Dictionary<string, int>();
+            int total = 0;
+            string input;
+            while ((input = Console.ReadLine()) != null && input != "")
+            {
+                if (trees.ContainsKey(input))
+                    trees[input]++;
+                else
+                    trees[input] = 1;
+                total++;
+
+            }
+            var sortedTrees = trees.OrderBy(x => x.Key).ToList(); ;
+            foreach (var tree in sortedTrees)
+            {
+                double per = tree.Value / (double)total * 100;
+                Console.WriteLine($"{tree.Key} {per:F4}");
+            }
+        }
+    }
+}
