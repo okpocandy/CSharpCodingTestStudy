498bff990d53072d209538600b099b56b89c6481
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-13 01:54:59 +0900

    백준문제 4889, 9933 추가
    백준문제 4889, 9933 추가

---
+++ cSharp/문자열/4889.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _4889
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while(true)
+            {
+                string s = Console.ReadLine();
+                if (s[0] == '-')
+                    break;
+                Stack<char> st = new Stack<char>();
+                int rc = 0;
+                foreach(var c in s)
+                {
+                    if(c == '{')
+                    {
+                        st.Push(c);
+                    }
+                    else
+                    {
+                        if (st.Count > 0)
+                            st.Pop();
+                        else
+                        {
+                            rc++;
+                            st.Push('{');
+                        }
+                            
+                    }
+                }
+                rc += st.Count / 2;
+                Console.WriteLine($"{index}. {rc}");
+                index++;
+            }
+        }
+    }
+}
---
+++ cSharp/문자열/9933.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _9933
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> ls = new List<string>();   
+            for(int i=0; i<n; i++)
+            {
+                ls.Add(Console.ReadLine());
+            }
+            foreach(var s in ls)
+            {
+                string tmp = new string(s.Reverse().ToArray());
+                if (ls.Contains(tmp))
+                {
+                    Console.WriteLine(tmp.Length + " " + tmp[tmp.Length / 2]);
+                    break;
+                }
+            }
+        }
+    }
+}
