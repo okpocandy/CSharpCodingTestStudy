d3e134e283f138e6450b8588cc66778c9aab9b6e
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-14 02:19:41 +0900

    백준문제 1159, 1212 추가
    백준문제 1159, 1212 추가

---
+++ cSharp/문자열/1159.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _1159
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<char, int> firstNameCount = new Dictionary<char, int>();
+
+            // 입력을 읽고 첫 글자별 카운팅
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                char firstChar = name[0];
+
+                if (firstNameCount.ContainsKey(firstChar))
+                    firstNameCount[firstChar]++;
+                else
+                    firstNameCount[firstChar] = 1;
+            }
+
+            // 선발 가능한 첫 글자들을 저장
+            List<char> selected = new List<char>();
+
+            foreach (var entry in firstNameCount)
+            {
+                if (entry.Value >= 5)
+                    selected.Add(entry.Key);
+            }
+
+            // 결과 출력
+            if (selected.Count == 0)
+            {
+                Console.WriteLine("PREDAJA");
+            }
+            else
+            {
+                selected.Sort();  // 사전순 정렬
+                Console.WriteLine(string.Join("", selected));
+            }
+        }
+    }
+}
---
+++ cSharp/문자열/1212.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _1212
+    {
+        static void Main(string[] args)
+        {
+            string octal = Console.ReadLine();
+
+            // StringBuilder를 이용해 결과를 저장
+            StringBuilder binary = new StringBuilder();
+
+            // 첫 번째 자리만 따로 변환 (앞의 0 제거)
+            binary.Append(Convert.ToString(octal[0] - '0', 2));
+
+            // 나머지 자리는 항상 3자리로 변환
+            for (int i = 1; i < octal.Length; i++)
+            {
+                binary.Append(Convert.ToString(octal[i] - '0', 2).PadLeft(3, '0'));
+            }
+
+            Console.WriteLine(binary.ToString());
+        }
+    }
+}
