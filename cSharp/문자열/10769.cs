95116df969a09b06ec85c33157da908fa1bab47c
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-15 01:16:59 +0900

    백준문제 10769, 2002 추가
    백준문제 10769, 2002 추가

---
+++ cSharp/문자열/10769.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _10769
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            bool dot = false;
+            bool h = false;
+            int index = 0;
+            int happy = 0;
+            int sad = 0;
+            while (index<s.Length)
+            {
+                if (s[index] == ':')
+                {
+                    if(index+2 < s.Length)
+                    {
+                        if (s[index+1] == '-')
+                        {
+                            if (s[index+2] == ')')
+                            {
+                                happy++;
+                            }
+                            else if(s[index+2] == '(')
+                            {
+                                sad++;
+                            }
+                        }
+                    }
+                }
+                index++;
+            }
+            if(happy == sad)
+            {
+                if (happy == 0 && sad == 0)
+                    Console.WriteLine("none");
+                else
+                    Console.WriteLine("unsure");
+            }
+            else if (happy < sad)
+                Console.WriteLine("sad");
+            else
+                Console.WriteLine("happy");
+        }
+    }
+}
---
+++ cSharp/문자열/2002.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _2002
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            // 차량 입구 순서 저장
+            Dictionary<string, int> entranceOrder = new Dictionary<string, int>();
+            for (int i = 0; i < n; i++)
+            {
+                string car = Console.ReadLine();
+                entranceOrder[car] = i;  // 차량 번호와 입구 순서 저장
+            }
+
+            // 차량 출구 순서 저장
+            List<int> exitOrder = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                string car = Console.ReadLine();
+                exitOrder.Add(entranceOrder[car]);  // 출구 순서에 해당하는 입구 인덱스 추가
+            }
+
+            // 추월한 차량 수 계산 (좌측의 작은 수가 뒤쪽에 나오면 추월)
+            int overtakeCount = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (exitOrder[i] > exitOrder[j])  // 추월 발생
+                    {
+                        overtakeCount++;
+                        break;  // 해당 차량에서 추월이 확인되면 다음으로 이동
+                    }
+                }
+            }
+
+            Console.WriteLine(overtakeCount);
+        }
+    }
+}
