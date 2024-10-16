51a0a0778d0f55ce3edf6a72d38a20efac3955f2
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-16 22:11:15 +0900

    백준문제 6996, 8595 추가
    백준문제 6996, 8595 추가

---
+++ cSharp/문자열/6996.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _6996
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i=0; i<n; i++)
+            {
+                string[] ab = Console.ReadLine().Split();
+                string a = ab[0];
+                string b= ab[1];
+                char[] ac = a.ToCharArray();
+                char[] bc = b.ToCharArray();
+                Array.Sort(ac);
+                Array.Sort(bc);
+                bool ans = true;
+                if(ac.Length == bc.Length)
+                {
+                    int c = Math.Min(ac.Length, bc.Length);
+                    for (int j = 0; j < c; j++)
+                    {
+                        if (ac[j] == bc[j])
+                            continue;
+                        ans = false;
+                    }
+                    if (ans)
+                        Console.WriteLine($"{a} & {b} are anagrams.");
+                    else
+                        Console.WriteLine($"{a} & {b} are NOT anagrams.");
+                }
+                else
+                    Console.WriteLine($"{a} & {b} are NOT anagrams.");
+
+            }
+        }
+    }
+}
---
+++ cSharp/문자열/8595.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _8595
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            long ans = 0;  // 히든 넘버의 합 (long 사용)
+
+            string tmp = "";  // 현재 숫자를 저장할 임시 변수
+
+            for (int i = 0; i < n; i++)
+            {
+                if (char.IsDigit(s[i]))  // 현재 문자가 숫자인지 확인
+                {
+                    tmp += s[i];  // 숫자라면 tmp에 추가
+                }
+                else if (tmp != "")  // 숫자열이 끝났다면
+                {
+                    ans += long.Parse(tmp);  // 합산
+                    tmp = "";  // 임시 변수 초기화
+                }
+            }
+
+            // 마지막에 남아 있는 숫자 처리
+            if (tmp != "")
+            {
+                ans += long.Parse(tmp);
+            }
+
+            Console.WriteLine(ans);  // 결과 출력
+        }
+    }
+}
