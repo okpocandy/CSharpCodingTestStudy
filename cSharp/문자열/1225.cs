538dbff22d859db7f72854c6cc621a3b111bbcf5
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-17 20:56:42 +0900

    백준문제 1225, 1371 추가
    백준문제 1225, 1371 추가

---
+++ cSharp/문자열/1225.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    class _1225
+    {
+        static void Main(string[] args)
+        {
+            string[] ss = Console.ReadLine().Split();
+            string a= ss[0];
+            string b= ss[1];
+            long ans = 0;
+            for(int i=0; i<a.Length; i++)
+            {
+                for(int j=0; j<b.Length; j++)
+                {
+                    ans += int.Parse(a[i].ToString())
+                            * int.Parse(b[j].ToString());
+                }
+            }
+            Console.WriteLine(ans);
+
+        }
+    }
+}
---
+++ cSharp/문자열/1371.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _1371
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                foreach(char c in line)
+                {
+                    if(char.IsLower(c))
+                    {
+                        if (dic.ContainsKey(c))
+                            dic[c]++;
+                        else
+                            dic[c] = 1;
+                    }
+                    
+                }
+            }
+            int maxc = dic.Values.Max();
+            var result = from item in dic
+                         where item.Value == maxc
+                         orderby item.Key
+                         select item.Key;
+            string ans = "";
+            foreach(char c in result)
+            {
+                if (c != ' ')
+                    ans += c;
+            }
+            Console.WriteLine(ans);
+
+            
+        }
+    }
+}
