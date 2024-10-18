9dfb5d9afd43ebe52d70479c29ecc61c8bed3471
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-18 18:44:17 +0900

    백준 문제 3062 추가
    백준 문제 3062 추가

---
+++ cSharp/문자열/3062.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    class _3062
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for(int i=0; i<t; i++)
+            {
+                string s= Console.ReadLine();
+                string rs = new string(s.Reverse().ToArray());
+
+                int n = int.Parse(s) + int.Parse(rs);
+                string ans = n.ToString();
+                string rans = new string(ans.Reverse().ToArray());
+                if (ans == rans)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
+        }
+    }
+}
