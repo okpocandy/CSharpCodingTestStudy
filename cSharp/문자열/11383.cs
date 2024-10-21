156c59f2ac1ce3f5fe90dee0bd93de60d08129d4
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-21 23:30:45 +0900

    백준문제 11383, 9946 추가
    백준문제 11383, 9946 추가

---
+++ cSharp/문자열/11383.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _11383
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nm[0];
+            int m = nm[1];
+            
+            string[] strings = new string[n];
+            for (int i = 0; i < n; i++)
+                strings[i] = Console.ReadLine();
+            bool ans = true;
+            for (int i=0; i<n; i++)
+            {
+                string tmp = Console.ReadLine();
+                for(int j=0; j<m; j++)
+                {
+                    if(strings[i][j] == tmp[2*j] && strings[i][j] == tmp[2*j+1])
+                        continue;
+                    else
+                    {
+                        ans = false;
+                        break;
+                    }
+                }
+            }
+            if (ans)
+                Console.WriteLine("Eyfa");
+            else
+                Console.WriteLine("Not Eyfa");
+        }
+    }
+}
---
+++ cSharp/문자열/9946.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _9946
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while(true)
+            {
+                string a = Console.ReadLine();
+                string b = Console.ReadLine();
+                if (a == "END" && b == "END")
+                    break;
+                char[] ca = a.ToCharArray();
+                char[] cb = b.ToCharArray();
+                Array.Sort(ca);
+                Array.Sort(cb);
+                string sa = new string(ca);
+                string sb = new string(cb);
+                if (sa == sb)
+                    Console.WriteLine($"Case {index}: same");
+                else
+                    Console.WriteLine($"Case {index}: different");
+                index++;
+            }
+        }
+    }
+}
