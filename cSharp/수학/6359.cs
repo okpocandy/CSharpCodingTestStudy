c79b6f9672fcd5f6a082ff68fc044a8409757b62
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-24 01:15:54 +0900

    백준 문제 6359, 9506 추가
    백준 문제 6359, 9506 추가

---
+++ cSharp/수학/6359.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    class _6359
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int tmp = int.Parse(Console.ReadLine());
+                int a = 3;
+                while (tmp >= a * a)
+                {
+                    a++;
+                }
+                Console.WriteLine(a - 1);
+            }
+        }
+    }
+}
---
+++ cSharp/수학/9506.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _9506
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n == -1)
+                    break;
+                int sum = 0;
+                StringBuilder sb = new StringBuilder();
+                List<int> list = new List<int>();
+                sb.Append($"{n} =");
+                for (int i = 1; i < n; i++)
+                {
+                    if(n%i == 0)
+                    {
+                        list.Add(i);
+                        sum += i;
+                    }
+                }
+                if (sum == n)
+                {
+                    foreach(int i in list)
+                        sb.Append($" {i} +");
+                    Console.WriteLine(sb.ToString()[..^2]);
+                }
+                else
+                    Console.WriteLine($"{n} is NOT perfect.");
+
+            }
+        }
+    }
+}
