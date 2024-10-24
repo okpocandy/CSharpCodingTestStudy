5594a38ebda6000f1916f4e9e248697e55f2531e
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-25 01:09:10 +0900

    백준 2485, 5533 추가
    백준 2485, 5533 추가

---
+++ cSharp/수학/2485.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _2485
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();   
+            for(int i=0; i<n; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+            List<int> sum = new List<int>();
+            for(int i=0; i<n-1; i++)
+            {
+                sum.Add(list[i + 1] - list[i]);
+            }
+            int gcd = sum[0];
+            for (int i = 1; i < n - 1; i++)
+                gcd = Gcd(gcd, sum[i]);
+            int cnt = 0;
+            for (int i = 0; i < n - 1; i++)
+                cnt += (sum[i] / gcd) - 1;
+            Console.WriteLine(cnt);
+        }
+
+        static private int Gcd(int a, int b)
+        {
+            if (a % b == 0)
+                return b;
+            else
+                return Gcd(b, a % b);
+        }
+    }
+}
---
+++ cSharp/수학/5533.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _5533
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int[]> ints = new List<int[]>();
+            for(int i=0; i<n; i++)
+            {
+                ints.Add(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+            }
+            int[] sum = new int[n];
+            for(int i=0; i<3; i++)
+            {
+                Dictionary<int, int> dic = new Dictionary<int, int>();
+                foreach (var j in ints)
+                {
+                    if (dic.ContainsKey(j[i]))
+                        dic[j[i]]++;
+                    else
+                        dic[j[i]] = 1;
+                }
+                for(int j=0; j<n; j++)
+                {
+                    if (dic[ints[j][i]] == 1)
+                        sum[j] += ints[j][i];
+                }
+            }
+            foreach(var i in sum)
+                Console.WriteLine(i);
+            
+            
+        }
+    }
+}
