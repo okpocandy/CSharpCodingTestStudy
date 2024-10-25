a05280576e5c41351f35a5831bedb4b4288c5990
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-10-25 19:35:36 +0900

    백준문제 4673 추가
    백준문제 4673 추가

---
+++ cSharp/수학/4673.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.수학
+{
+    internal class _4673
+    {
+        static void Main(string[] args)
+        {
+            string[] ans = Console.ReadLine().Split();
+            string a = ans[0];
+            string b = ans[1];
+            string ra = new string(a.Reverse().ToArray());
+            string rb = new string(b.Reverse().ToArray());
+            int ia = int.Parse(ra);
+            int ib = int.Parse(rb);
+
+            Console.WriteLine(Math.Max(ia, ib));
+        }
+        
+    }
+}
