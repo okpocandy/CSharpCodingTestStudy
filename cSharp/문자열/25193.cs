1e51912fdb58c165a53e4a35bad56db51d9bcc4f
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-11-04 22:14:42 +0900

    백준문제 25193 추가
    백준문제 25193 추가

---
+++ cSharp/문자열/25193.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    internal class _25193
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            int cnt = 0;
+            int ch = 0;
+            foreach(var c in s)
+            {
+                if (c != 'C')
+                    cnt++;
+                else
+                    ch++;
+
+            }
+            if (ch % (cnt + 1) != 0) Console.WriteLine(ch / (cnt + 1) + 1);
+            else Console.WriteLine(ch / (cnt + 1));
+        }
+    }
+}
