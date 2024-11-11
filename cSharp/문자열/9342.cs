f7028a737cfb1a037bfa216c57f88706cfb89f2e
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-11-11 22:25:46 +0900

    백준문제 9342 추가
    백준문제 9342 추가

---
+++ cSharp/문자열/9342.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.문자열
+{
+    class _9342
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string pattern = @"^[ABCDEF]?A+F+C+[ABCDEF]?$";
+            for (int i=0; i<n; i++)
+            {
+                string input = Console.ReadLine();
+                if (Regex.IsMatch(input, pattern))
+                    Console.WriteLine("Infected!");
+                else
+                    Console.WriteLine("Good");
+
+            }
+        }
+    }
+}
