758fb2728dcdfc5a4c5f911665107d353579a52e
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-11-14 00:22:46 +0900

    백준문제 1340 추가
    백준문제 1340 추가

--- cSharp/문자열/1340.cs
+++ cSharp/문자열/1340.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
-namespace C_Sharp_Study.cSharp.문자열
+namespace C_Sharp_Study
 {
-    class _1340
+    internal class YearProgress
     {
+        static void Main(string[] args)
+        {
+            // 입력 파싱
+            string input = Console.ReadLine();
+            DateTime currentTime = DateTime.ParseExact(input, "MMMM dd, yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            // 현재 연도 시작과 끝 날짜
+            DateTime startOfYear = new DateTime(currentTime.Year, 1, 1, 0, 0, 0);
+            DateTime endOfYear = new DateTime(currentTime.Year + 1, 1, 1, 0, 0, 0);
+
+            // 경과 시간 비율 계산
+            double totalMinutesInYear = (endOfYear - startOfYear).TotalMinutes;
+            double minutesElapsed = (currentTime - startOfYear).TotalMinutes;
+            double percentage = (minutesElapsed / totalMinutesInYear) * 100;
+
+            // 결과 출력
+            Console.WriteLine(percentage);
+        }
     }
 }
