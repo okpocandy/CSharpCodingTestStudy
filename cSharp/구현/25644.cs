eb576fd1b84afe07f6f26d19523cb1045a0fd3f1
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-11-19 23:44:56 +0900

    백준문제 25644 추가
    백준문제 25644 추가

---
+++ cSharp/구현/25644.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] prices = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        int minPrice = int.MaxValue;
+        int maxProfit = 0;
+
+        foreach (int price in prices)
+        {
+            // 최소 주가 갱신
+            if (price < minPrice)
+                minPrice = price;
+
+            // 최대 이익 갱신
+            int profit = price - minPrice;
+            if (profit > maxProfit)
+                maxProfit = profit;
+        }
+
+        Console.WriteLine(maxProfit);
+    }
+}
