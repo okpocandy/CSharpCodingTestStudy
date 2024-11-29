bb604aad08047c0e3dc757d838bb297a45c3e035
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-11-29 15:17:48 +0900

    백준문제 11170, 9455 추가
    백준문제 11170, 9455 추가

---
+++ cSharp/구현/11170.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class CountZero
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int n = int.Parse(input[0]);
+                int m = int.Parse(input[1]);
+
+                int count = 0;
+
+                // N부터 M까지 모든 숫자 확인
+                for (int num = n; num <= m; num++)
+                {
+                    string numStr = num.ToString();
+                    foreach (char c in numStr)
+                    {
+                        if (c == '0')
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
---
+++ cSharp/구현/9455.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    class _9455
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for(int i=0; i<t; i++)
+            {
+                int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                List<int[]> ls = new List<int[]>();
+                int ans = 0;
+                for (int j = 0; j < ints[0]; j++)
+                {
+                    ls.Add(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+                }
+                for(int k=0; k < ints[1];  k++)
+                {
+                    int cnt = 0;
+                    for (int l=ints[0]-1; l >= 0;  l--)
+                    {
+                        
+                        if (ls[l][k] == 1)
+                        {
+                            ans += ints[0] - 1 - l - cnt;
+                            cnt++;
+                        }
+                    }
+
+                }
+                Console.WriteLine(ans);
+            }
+            
+            
+            
+        }
+    }
+}
