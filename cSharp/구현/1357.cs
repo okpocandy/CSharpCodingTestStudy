e93413bf0c392c69b957100fcac2cd818b13c7af
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-12-04 22:22:42 +0900

    백준문제 1357 추가

---
+++ cSharp/구현/1357.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    class _1357
+    {
+        static void Main(string[] args)
+        {
+            string[] xy = Console.ReadLine().Split();
+            string x = xy[0];
+            string y = xy[1];
+            string rx = new string(x.Reverse().ToArray());
+            string ry = new string(y.Reverse().ToArray());
+            int irx = int.Parse(rx);
+            int iry = int.Parse(ry);
+            string ans = new string((irx + iry).ToString().Reverse().ToArray());
+            Console.WriteLine(int.Parse(ans));
+        }
+    }
+}
