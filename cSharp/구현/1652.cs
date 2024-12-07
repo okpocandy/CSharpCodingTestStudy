d4e19be5c502adc5e6b37a78d531e98cad80bd31
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-12-07 17:07:04 +0900

    백준문제 1652, 2846 추가
    백준문제 1652, 2846 추가

---
+++ cSharp/구현/1652.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        char[,] room = new char[n, n];
+
+        // 방 구조 입력
+        for (int i = 0; i < n; i++)
+        {
+            string line = Console.ReadLine();
+            for (int j = 0; j < n; j++)
+            {
+                room[i, j] = line[j];
+            }
+        }
+
+        int horizontal = 0; // 가로로 누울 수 있는 자리
+        int vertical = 0;   // 세로로 누울 수 있는 자리
+
+        // 가로 방향 자리 계산
+        for (int i = 0; i < n; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (room[i, j] == '.') count++;
+                else count = 0;
+
+                if (count == 2) horizontal++; // 2칸 이상 연속되면 자리 추가
+            }
+        }
+
+        // 세로 방향 자리 계산
+        for (int j = 0; j < n; j++)
+        {
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (room[i, j] == '.') count++;
+                else count = 0;
+
+                if (count == 2) vertical++; // 2칸 이상 연속되면 자리 추가
+            }
+        }
+
+        // 결과 출력
+        Console.WriteLine($"{horizontal} {vertical}");
+    }
+}
---
+++ cSharp/구현/2846.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    class _2846
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int bef = ints[0];
+            int max = 0;
+            if (n == 1)
+                Console.WriteLine(0);
+            else
+            {
+                int cnt = 0;
+                for(int i=1; i<n; i++)
+                {
+                    if (ints[i] > bef)
+                    {
+                        cnt += ints[i] - bef;
+                    }
+                    else
+                    {
+                        cnt = 0;
+                    }
+                    bef = ints[i];
+                    max = Math.Max(max, cnt);
+                }
+                Console.WriteLine(max);
+            }
+        }
+    }
+}
