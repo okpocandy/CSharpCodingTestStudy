b19848a0b7a4ffd39e309b4bb98fecf3ef392b59
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-12-08 22:32:14 +0900

    백준문제 16960 추가
    백준문제 16960 추가

---
+++ cSharp/구현/16960.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    class _16960
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            List<int[]> graph = new List<int[]>();
+            int ans = 0;
+            for(int i=0; i < nm[0]; i++)
+            {
+                graph.Add(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+            }
+            for(int k=0; k < nm[0];  k++)
+            {
+                HashSet<int> itns = new HashSet<int>();
+                for (int i = 0; i < nm[0]; i++)
+                {
+                    if (i == k)
+                        continue;
+                    for (int j = 1; j < graph[i].Length; j++)
+                    {
+                        itns.Add(graph[i][j]);
+                    }
+                }
+                if(itns.Count == nm[1])
+                {
+                    ans = 1;
+                }
+            }
+            Console.WriteLine(ans);
+            
+        }
+    }
+}
