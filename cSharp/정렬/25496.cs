aeccc5cca246562641d632ca1d2970f2dc4b9c0e
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-12-14 22:19:52 +0900

    백준문제 25496 추가
    백준문제 25496 추가

---
+++ cSharp/정렬/25496.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _25496
+    {
+        static void Main(string[] args)
+        {
+            int[] pn = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int p = pn[0];
+            int n = pn[1];
+            int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(ints);
+            int cnt = 0;
+            while(p < 200)
+            {
+                p += ints[cnt];
+                cnt++;
+                if (cnt == n)
+                    break;
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
