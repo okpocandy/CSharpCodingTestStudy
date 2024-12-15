e091747878a368267da78780249a3762145a816c
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-12-16 00:28:36 +0900

    백준문제 10867, 2822 추가
    백준문제 10867, 2822 추가

---
+++ cSharp/정렬/10867.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _10867
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            HashSet<int> intset = new HashSet<int>();
+            foreach(int i in ints)
+                intset.Add(i);
+            int[] ans = intset.ToArray();
+            Array.Sort(ans);
+            Console.Write(string.Join(" ", ans));
+                
+        }
+    }
+}
---
+++ cSharp/정렬/2822.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _2822
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for(int i=0; i<8;  i++)
+            {
+                dic[i+1] = int.Parse(Console.ReadLine());
+            }
+
+            var sorted_dic = dic
+                .OrderByDescending(x => x.Value)
+                .Take(5)
+                .OrderBy(x => x.Key)
+                .ToArray();
+
+            int ans = sorted_dic.Sum(x=> x.Value);
+            Console.WriteLine(ans);
+            Console.WriteLine(string.Join(" ", sorted_dic.Select(x=>x.Key)));
+
+        }
+    }
+}
