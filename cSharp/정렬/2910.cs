63d080089535dc8f2c0f73245a7283c4b2b4a84b
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-12-16 22:41:16 +0900

    백준문제 2910 추가
    백준문제 2910 추가

---
+++ cSharp/정렬/2910.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _2910
+    {
+        static void Main(string[] args)
+            {
+                int[] nc = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int n = nc[0];
+                int c = nc[1];
+                Dictionary<int, int> dic = new Dictionary<int, int>();
+                int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                foreach(int i in ints)
+                {
+                    if(dic.ContainsKey(i))
+                        dic[i]++;
+                    else
+                        dic[i] = 1;
+                }
+                var sorted_dic = dic
+                .OrderByDescending(x => x.Value);
+                foreach(var i in sorted_dic)
+                {
+                    for(int j=0; j<i.Value; j++)
+                    {
+                        Console.Write(i.Key + " ");
+                    }
+                }
+            }
+    }
+}
