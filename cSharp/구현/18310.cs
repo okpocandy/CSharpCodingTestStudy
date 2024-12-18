d331dbdb1b362c0cc894abd9aa8c76c92b77cd3e
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-12-18 19:40:59 +0900

    백준문제 1830 추가
    백준문제 1830 추가

---
+++ cSharp/구현/18310.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _18310
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] houses = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(houses);
+
+            // 중앙값을 찾는 방식으로 최적화
+            int median = houses[(n - 1) / 2];
+            Console.WriteLine(median);
+        }
+    }
+}
