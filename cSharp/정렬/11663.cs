de2d04f37ca4c4857efaed84a3d43fb8f9a911c7
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-12-25 20:53:29 +0900

    백준문제 11663, 17390 추가
    백준문제 11663, 17390 추가

---
+++ cSharp/정렬/11663.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _11663
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nm[0];
+            int m = nm[1];
+
+            int[] points = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(points); // 점 정렬
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < m; i++)
+            {
+                int[] line = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int start = line[0];
+                int end = line[1];
+
+                // 범위 내 점의 개수 계산
+                int lowerBound = LowerBound(points, start);
+                int upperBound = UpperBound(points, end);
+
+                sb.AppendLine((upperBound - lowerBound).ToString());
+            }
+
+            Console.Write(sb.ToString());
+        }
+
+        static int LowerBound(int[] arr, int target)
+        {
+            int left = 0, right = arr.Length;
+
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                if (arr[mid] < target) left = mid + 1;
+                else right = mid;
+            }
+
+            return left;
+        }
+
+        static int UpperBound(int[] arr, int target)
+        {
+            int left = 0, right = arr.Length;
+
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                if (arr[mid] <= target) left = mid + 1;
+                else right = mid;
+            }
+
+            return left;
+        }
+    }
+}
---
+++ cSharp/정렬/17390.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _17390
+    {
+        static void Main(string[] args)
+        {
+            int[] nq = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nq[0];
+            int q = nq[1];
+
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(a); // 비내림차순 정렬
+
+            // Prefix Sum 배열 생성
+            int[] prefixSum = new int[n + 1];
+            for (int i = 1; i <= n; i++)
+            {
+                prefixSum[i] = prefixSum[i - 1] + a[i - 1];
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            // 쿼리 처리
+            for (int i = 0; i < q; i++)
+            {
+                int[] lr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int l = lr[0];
+                int r = lr[1];
+
+                // 구간 합 계산
+                int sum = prefixSum[r] - prefixSum[l - 1];
+                sb.AppendLine(sum.ToString());
+            }
+
+            Console.Write(sb.ToString());
+        }
+    }
+}
