8d79eade0ebd84bbd1078e9beda47bd42dd2e5a6
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-12-28 21:25:29 +0900

    백준문제 2304, 2477 추가
    백준문제 2304, 2477 추가

---
+++ cSharp/구현/2304.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _2304
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<(int l, int h)> ls = new List<(int l, int h)>();
+            int ans = 0;
+            for(int i=0; i<n; i++)
+            {
+                int[] lh = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int l = lh[0];
+                int h = lh[1];
+                ls.Add((l, h));
+            }
+            int maxH = ls.Max(x => x.h);
+            ls.Sort();
+            List<int> maxHs = new List<int>();
+            for(int i=0; i<n; i++)
+            {
+                if (ls[i].h == maxH)
+                {
+                    maxHs.Add(i);
+                }
+            }
+            
+            maxHs.Sort();
+            int stmh = maxHs.First();
+            int emh = maxHs.Last();
+            ans += (ls[emh].l - ls[stmh].l + 1) * maxH;
+
+            int preL = ls.First().l;
+            int preH = ls.First().h;
+            for(int i=0; i<=stmh; i++)
+            {
+                int nowL = ls[i].l;
+                int nowH = ls[i].h;
+                if (ls[i].h > preH)
+                {
+                    ans += (nowL - preL) * preH;
+                    preL = nowL;
+                    preH = nowH;
+                }
+            }
+            preL = ls.Last().l;
+            preH = ls.Last().h;
+            for(int i=n-1; i>=emh; i--)
+            {
+                int nowL = ls[i].l;
+                int nowH = ls[i].h;
+                if (ls[i].h > preH)
+                {
+                    ans += (preL - nowL) * preH;
+                    preL = nowL;
+                    preH = nowH;
+                }
+            }
+            Console.WriteLine(ans);
+            
+            
+            
+        }
+    }
+}
---
+++ cSharp/구현/2477.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _2477
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            int[] directions = new int[6];
+            int[] lengths = new int[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                directions[i] = int.Parse(input[0]);
+                lengths[i] = int.Parse(input[1]);
+            }
+
+            int maxWidth = 0, maxHeight = 0;
+            int minWidth = 0, minHeight = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (directions[i] == 1 || directions[i] == 2) // 동/서 방향
+                {
+                    maxWidth = Math.Max(maxWidth, lengths[i]);
+                }
+                else if (directions[i] == 3 || directions[i] == 4) // 남/북 방향
+                {
+                    maxHeight = Math.Max(maxHeight, lengths[i]);
+                }
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (lengths[i] + lengths[(i + 2) % 6] == maxWidth || lengths[i] + lengths[(i + 2) % 6] == maxHeight)
+                {
+                    minWidth = lengths[(i + 1) % 6];
+                    minHeight = lengths[(i + 2) % 6];
+                    break;
+                }
+            }
+
+            int area = (maxWidth * maxHeight) - (minWidth * minHeight);
+            Console.WriteLine(area * k);
+        }
+    }
+}
