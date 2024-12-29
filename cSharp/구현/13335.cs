a3ff5eaa8eb1f94ebf7f61033fe724d226751335
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-12-29 22:05:34 +0900

    백준문제 13335, 1713 추가
    백준문제 13335, 1713 추가

---
+++ cSharp/구현/13335.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class TruckBridge
+    {
+        static void Main(string[] args)
+        {
+            string[] firstLine = Console.ReadLine().Split();
+            int n = int.Parse(firstLine[0]);
+            int w = int.Parse(firstLine[1]);
+            int L = int.Parse(firstLine[2]);
+
+            int[] trucks = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            Queue<int> bridge = new Queue<int>();
+            int currentWeight = 0;
+            int time = 0;
+
+            for (int i = 0; i < w; i++) bridge.Enqueue(0); 
+
+            int index = 0;
+            while (index < n || currentWeight > 0)
+            {
+                time++;
+
+               
+                currentWeight -= bridge.Dequeue();
+
+             
+                if (index < n && currentWeight + trucks[index] <= L)
+                {
+                    bridge.Enqueue(trucks[index]);
+                    currentWeight += trucks[index];
+                    index++;
+                }
+                else
+                {
+                   
+                    bridge.Enqueue(0);
+                }
+            }
+
+            Console.WriteLine(time);
+        }
+    }
+}
---
+++ cSharp/구현/1713.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _1713
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int[] students = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for(int i=1; i<=100; i++)
+            {
+                dic[i] = 0;
+            }
+            List<int> ls = new List<int>();
+            foreach(int student in students)
+            {
+                if (ls.Count < n && !ls.Contains(student))
+                {
+                    ls.Add(student);
+                    dic[student]++;
+                }
+
+                else if(ls.Count>=n && !ls.Contains(student))
+                {
+                    int delSut = ls.First();
+                    foreach(var i in ls)
+                    {
+                        if (dic[i] < dic[delSut])
+                        {
+                            delSut = i;
+                        }
+                    }
+                    ls.Remove(delSut);
+                    dic[delSut] = 0;
+                    ls.Add(student);
+                    dic[student]++;
+                }
+                else if(ls.Contains(student))
+                    dic[student]++;
+            }
+            ls.Sort();
+            foreach(int student in ls)
+                Console.WriteLine(student);
+        }
+    }
+}
