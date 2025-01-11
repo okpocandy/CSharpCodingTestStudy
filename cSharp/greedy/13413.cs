63dc985db64cd3621cb8846ebe8ae2228999db25
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-01-11 22:37:12 +0900

    백준문제 13413, 16237 추가
    백준문제 13413, 16237 추가

---
+++ cSharp/greedy/13413.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class 오셀로_재배치
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int test = 0; test < t; test++)
+            {
+                int n = int.Parse(Console.ReadLine()); 
+                string initial = Console.ReadLine(); 
+                string target = Console.ReadLine(); 
+
+                int w_to_b = 0;
+                int b_to_w = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (initial[i] == 'W' && target[i] == 'B')
+                        w_to_b++;
+                    else if (initial[i] == 'B' && target[i] == 'W')
+                        b_to_w++;
+                }
+                int swaps = Math.Min(w_to_b, b_to_w); 
+                int flips = Math.Abs(w_to_b - b_to_w);
+                Console.WriteLine(swaps + flips); 
+            }
+        }
+    }
+}
---
+++ cSharp/greedy/16237.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.greedy
+{
+    internal class _16237
+    {
+        static void Main(string[] args)
+        {
+            // 바구니에는 최대 5kg
+            // 필요바구니 최소
+            //    12345
+            int[] abcde = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int ans = 0;
+            ans += abcde[4];
+            ans += abcde[3];
+            abcde[0] -= abcde[3];
+            ans += abcde[2];
+            while (abcde[2] > 0)
+            {
+                if (abcde[1] > 0)
+                    abcde[1]--;
+                else if (abcde[0] > 0)
+                {
+                    abcde[0] -= 2;
+                }
+                abcde[2]--;
+            }
+            int tmp = abcde[1] / 2;
+            ans += tmp;
+            abcde[1] -= tmp*2;
+            abcde[0] -= tmp;
+            if (abcde[1] == 1)
+            {
+                ans++;
+                abcde[0] -= 3;
+            }
+            if (abcde[0] > 0)
+                ans += (abcde[0] + 4) / 5;
+            Console.WriteLine(ans);
+        }
+    }
+}
