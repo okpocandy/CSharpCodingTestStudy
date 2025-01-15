8ab0631bdc819125a00d8ec477400fffbb22da73
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-01-16 01:49:16 +0900

    백준문제 29813 추가
    백준문제 29813 추가

---
+++ cSharp/구현/29813.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _29813
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<(string, int)> q = new Queue<(string, int)>();
+            for(int i=0; i<n; i++)
+            {
+                string[] inputs = Console.ReadLine().Split();
+                q.Enqueue((inputs[0], int.Parse(inputs[1])));
+            }
+            while(q.Count>1)
+            {
+                (string name, int num) = q.Dequeue();
+                for(int i=0; i<num-1; i++)
+                {
+                    q.Enqueue(q.Dequeue());
+                }
+                q.Dequeue();
+            }
+            Console.WriteLine(q.Dequeue().Item1);
+
+        }
+    }
+}
