9413fdbf32dee77326446949487bd4cc088df77a
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-01-17 02:26:52 +0900

    백준문제 14468 추가
    백준문제 14468 추가

---
+++ cSharp/구현/14468.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        int result = 0;
+
+
+        for (char outer = 'A'; outer <= 'Z'; outer++)
+        {
+            int firstIndex = input.IndexOf(outer);
+            int secondIndex = input.LastIndexOf(outer);
+
+            if (firstIndex == -1 || secondIndex == -1) continue;
+
+            var betweenCounts = new Dictionary<char, int>();
+            for (int i = firstIndex + 1; i < secondIndex; i++)
+            {
+                char current = input[i];
+                if (!betweenCounts.ContainsKey(current))
+                    betweenCounts[current] = 0;
+                betweenCounts[current]++;
+            }
+
+            foreach (var count in betweenCounts.Values)
+            {
+                if (count == 1)
+                    result++;
+            }
+        }
+
+        Console.WriteLine(result / 2);
+    }
+}
