9dc430d00bc2f94927626d31995cad043a02fb50
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-01-20 00:04:19 +0900

    백준문제 19941, 2824 추가
    백준문제 19941, 2824 추가

---
+++ cSharp/greedy/19941.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace C_Sharp_Study.cSharp.greedy
+{
+    internal class _19941
+    {
+        static void Main(string[] args)
+        {
+            int[] nk = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nk[0];
+            int k = nk[1];
+            char[] arrange = Console.ReadLine().ToCharArray();
+            int ans = 0;
+
+            for (int i = 0; i < arrange.Length; i++)
+            {
+                if (arrange[i] == 'P') 
+                {
+                   
+                    int start = Math.Max(0, i - k);
+                    int end = Math.Min(n - 1, i + k);
+
+                    for (int j = start; j <= end; j++)
+                    {
+                        if (arrange[j] == 'H') 
+                        {
+                            arrange[j] = 'E'; 
+                            ans++;
+                            break; 
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ cSharp/구현/2824.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _2824
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int m = int.Parse(Console.ReadLine());
+            int[] b = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            BigInteger am = 1;
+            BigInteger bm = 1;
+            for (int i = 0; i < n; i++)
+            {
+                am *= a[i];
+            }
+            for (int i = 0; i < m; i++)
+            {
+                bm *= b[i];
+            }
+            BigInteger ans = Gcd(am, bm);
+            string ansStr = ans.ToString();
+            if (ansStr.Length > 9)
+            {
+                ansStr = ansStr.Substring(ansStr.Length - 9);
+            }
+            Console.WriteLine($ansStr);
+
+        }
+
+        static BigInteger Gcd(BigInteger a, BigInteger b)
+        {
+            while (b != 0)
+            {
+                BigInteger c = b;
+
+                b = a % b;
+                a = c;
+            }
+            return a;
+        }
+    }
+}
