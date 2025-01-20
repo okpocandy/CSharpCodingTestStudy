3ae873ab0f499c77c9448f7a15b8a70e31c5d3d5
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-01-21 03:43:15 +0900

    백준문제 24523 추가
    백준문제 24523 추가

---
+++ cSharp/구현/24523.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _24523
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int[] result = new int[n];
+
+            int pre = ar[n-1];
+            List<int> ans = new List<int>();
+            int tmp = -2;
+            ans.Add(tmp+1);
+            for(int i=n-2; i>=0; i--)
+            {
+                if (ar[i] == pre)
+                {
+                    ans.Add(tmp+1);
+                }
+                else
+                {
+                    tmp = i + 1;
+                    pre = ar[i];
+                    ans.Add(tmp+1);
+                }
+            }
+            ans.Reverse();
+            Console.WriteLine(string.Join(" ", ans.ToArray()));
+        }
+    }
+}
