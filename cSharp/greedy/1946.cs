cb91973b6190f8adbb1fec981834984b0c4fc134
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-01-23 02:11:26 +0900

    백준문제 1946, 19368 추가
    백준문제 1946, 19368 추가

---
+++ cSharp/greedy/1946.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int testCases = int.Parse(Console.ReadLine());
+        for (int t = 0; t < testCases; t++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var applicants = new List<(int document, int interview)>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split();
+                int document = int.Parse(input[0]);
+                int interview = int.Parse(input[1]);
+                applicants.Add((document, interview));
+            }
+
+            applicants.Sort((a, b) => a.document.CompareTo(b.document));
+
+            int maxCount = 0;
+            int bestInterviewRank = int.MaxValue;
+
+            foreach (var applicant in applicants)
+            {
+                if (applicant.interview < bestInterviewRank)
+                {
+                    maxCount++;
+                    bestInterviewRank = applicant.interview;
+                }
+            }
+
+            Console.WriteLine(maxCount);
+        }
+    }
+}
---
+++ cSharp/구현/19368.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.구현
+{
+    internal class _19368
+    {
+        static void Main(string[] args)
+        {
+            int[] nht = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nht[0];
+            int h = nht[1];
+            int t = nht[2];
+            int maxt = t;
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            for(int i=0; i<n; i++)
+            {
+                int tmp = int.Parse(Console.ReadLine());
+                pq.Enqueue(tmp, -tmp);
+            }
+            while(t > 0)
+            {
+                var height = pq.Dequeue();
+                if (height < h)
+                {
+                    pq.Enqueue(height, -height); // 다시 큐에 삽입
+                    break;
+                }
+                if (height == 1)
+                {
+                    pq.Enqueue(height, -height);
+                    break;
+                }
+                    
+                height /= 2;
+                pq.Enqueue(height, -height);
+                t--;
+            }
+            int gi = pq.Dequeue();
+            if(gi >= h)
+            {
+                Console.WriteLine("NO");
+                Console.WriteLine(gi);
+            }
+            else
+            {
+                Console.WriteLine("YES");
+                Console.WriteLine(maxt - t);
+                
+            }
+        }
+    }
+}
