d3a0a05f792c0c5c45d80494365c49d388502154
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-01-26 01:46:57 +0900

    백준문제 1092, 2195 추가
    백준문제 1092, 2195 추가

---
+++ cSharp/greedy/1092.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<int> cranes = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).ToList();
+        int m = int.Parse(Console.ReadLine());
+        List<int> boxes = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).ToList();
+
+        if (boxes.Any(box => box > cranes[0]))
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+
+        int time = 0;
+        while (boxes.Count > 0)
+        {
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (index >= boxes.Count) break;
+                if (cranes[i] >= boxes[index])
+                {
+                    boxes.RemoveAt(index);
+                }
+                else
+                {
+                    index++;
+                    i--;
+                }
+            }
+            time++;
+        }
+
+        Console.WriteLine(time);
+    }
+}
---
+++ cSharp/greedy/2195.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace C_Sharp_Study.cSharp.greedy
+{
+    internal class _2195
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string p = Console.ReadLine();
+
+            int index = 0;
+            int ans = 0;
+
+            while (index < p.Length)
+            {
+                int maxMatch = 0;
+
+                for (int start = 0; start < s.Length; start++)
+                {
+                    int matchLength = 0;
+
+                    while (index + matchLength < p.Length &&
+                           start + matchLength < s.Length &&
+                           s[start + matchLength] == p[index + matchLength])
+                    {
+                        matchLength++;
+                    }
+
+                    maxMatch = Math.Max(maxMatch, matchLength);
+                }
+
+                index += maxMatch;
+                ans++;
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}
