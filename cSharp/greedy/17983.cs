c213976973e87e3378ead04061176f3e40dc947e
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-02-11 01:46:45 +0900

    백준 17983, 221 추가
    백준 17983, 221 추가

---
+++ cSharp/greedy/17983.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.greedy
+{
+    internal class _17983
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<(int, int)> ls = new List<(int, int)> ();
+            for(int i=0; i<n; i++)
+            {
+                int[] dt = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int d = dt[0];
+                int t = dt[1];
+                ls.Add((d,t));  
+            }
+            ls.Sort((a,b) => b.Item2.CompareTo(a.Item2));
+            int time = ls.First().Item2;
+            foreach(var i in ls)
+            {
+                int sd = i.Item1;
+                int st = i.Item2;
+                if(st <= time)
+                {
+                    time = st;
+                }
+                time -= sd;
+            }
+            Console.WriteLine(time);
+        }
+    }
+}
---
+++ cSharp/greedy/2212.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.greedy
+{
+    internal class _2212
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int[] sensors = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(sensors);
+            List<int> ls = new List<int>();
+            for(int i=0; i<n-1; i++)
+            {
+                ls.Add(Math.Abs(sensors[i+1] - sensors[i]));
+            }
+            ls.Sort((a,b) => b.CompareTo(a));
+            Console.WriteLine(ls.Skip(k - 1).Sum());
+
+        }
+    }
+}
