92b968b6b98e5976c3d547bbdf25c8279071de4b
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-02-11 01:46:32 +0900

    정렬 문제 추가
    정렬 문제 추가

---
+++ cSharp/정렬/1715.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _1715
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            for(int i=0; i<n; i++)
+            {
+                int tmp = int.Parse(Console.ReadLine());
+                pq.Enqueue(tmp, tmp);
+            }
+            long ans = 0;
+            while(pq.Count >= 2)
+            {
+                int a = pq.Dequeue();
+                int b= pq.Dequeue();
+                ans += a + b;
+                pq.Enqueue(a + b, a + b);
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ cSharp/정렬/18310_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _18310_2
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] houses = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            foreach(int house in houses)
+            {
+                int tmp = 0;
+                foreach(int difhou  in houses)
+                {
+                    tmp += Math.Abs(house - difhou);
+                }
+                dic[house] = tmp;
+            }
+            var sortedDic = dic.OrderBy(x => x.Value).ThenBy(x => x.Key);
+            Console.WriteLine(sortedDic.First().Key);
+        }
+    }
+}
