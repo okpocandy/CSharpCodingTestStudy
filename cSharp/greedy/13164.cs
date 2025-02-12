90bf2d137e67db72a357cc7e7c4b4ffd04f2d670
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-02-13 01:38:39 +0900

    백준 13164, 15553 추가
    백준 13164, 15553 추가

---
+++ cSharp/greedy/13164.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.greedy
+{
+    internal class _13164
+    {
+        static void Main(string[] args)
+        {
+            int[] nk = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nk[0];
+            int k = nk[1];
+            int[] kids = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            List<int> ls = new List<int>();
+            for (int i = 0; i < n - 1; i++)
+            {
+                ls.Add(kids[i + 1] - kids[i]);
+            }
+            
+            ls.Sort();
+            int[] ints = ls.ToArray();
+            
+            Console.WriteLine(ints[0..(n-k)].Sum());
+        }
+    }
+}
---
+++ cSharp/greedy/15553.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.greedy
+{
+    internal class _15553
+    {
+        static void Main(string[] args)
+        {
+            int[] nk = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nk[0];
+            int k = nk[1];
+            int[] difs = new int[n - 1];
+            List<int> ls = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                ls.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                difs[i] = ls[i + 1] - ls[i];
+            }
+            Array.Sort(difs);
+            Console.WriteLine(difs[0..(n - k)].Sum() + k);
+        }
+    }
+}
