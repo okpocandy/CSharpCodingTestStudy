bc68aaa4f8f9b61f0b6e0e29f703dd4a4e9a4414
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-02-14 00:30:38 +0900

    백준 19598, 21758 추가
    백준 19598, 21758 추가

---
+++ cSharp/greedy/19598.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.greedy
+{
+    internal class _19598
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<(int, int)> ls = new List<(int, int)> ();
+            for(int i=0; i<n; i++)
+            {
+                int[] st = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                ls.Add((st[0], st[1]));
+            }
+            ls.Sort((a,b) => a.Item1.CompareTo(b.Item1));
+            PriorityQueue<int, int> pq = new System.Collections.Generic.PriorityQueue<int, int>();
+            pq.Enqueue(ls.First().Item2, ls.First().Item2);
+            int ans = 1;
+            for(int i=1; i<n; i++)
+            {
+                int tmp = pq.Dequeue();
+                if (ls[i].Item1 < tmp)
+                {
+                    ans++;
+                    
+                    pq.Enqueue(tmp, tmp);
+                }
+                pq.Enqueue(ls[i].Item2, ls[i].Item2);
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ cSharp/greedy/21758.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.greedy
+{
+    internal class _21758
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] honeys = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int[] ssum = new int[100001];
+            ssum[0] = honeys[0];
+            int ans = 0;
+            for(int i=1; i<honeys.Length; i++)
+            {
+                ssum[i] = ssum[i-1] + honeys[i];
+            }
+            for (int i = 1; i < n - 1; i++)
+                ans = Math.Max(ans, ssum[n - 2] + ssum[i - 1] - honeys[i]);
+            for (int i = 1; i < n - 1; i++)
+                ans = Math.Max(ans, ssum[n - 1] - honeys[0] + ssum[n-1] - ssum[i] - honeys[i]);
+            for (int i = 1; i < n - 1; i++)
+                ans = Math.Max(ans, ssum[n - 2] - honeys[0] + honeys[i]);
+            Console.WriteLine(ans);
+        }
+    }
+}
