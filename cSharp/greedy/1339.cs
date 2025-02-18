64a56de62d29cca9f0695332cde55d40c4180b17
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-02-18 19:53:46 +0900

    백준 1339, 도서관 추가
    백준 1339, 도서관 추가

---
+++ cSharp/greedy/1339.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.greedy
+{
+    internal class _1339
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] words = new string[n];
+            Dictionary<char, int> nums = new Dictionary<char, int>();
+            nums['A'] = 10; nums['B'] = 10; nums['C'] = 10; nums['D'] = 10;
+            nums['E'] = 10; nums['F'] = 10; nums['G'] = 10; nums['H'] = 10;
+            nums['I'] = 10; nums['J'] = 10;
+            PriorityQueue<char, int> pq = new PriorityQueue<char, int>();
+            for (int i=0; i<n; i++)
+            {
+                string s= Console.ReadLine();
+                words[i] = s;
+                for(int j=0; j<s.Length; j++)
+                {
+                    pq.Enqueue(s[j], -(s.Length - j));
+                }
+            }
+            int maxnum = 9;
+            while(maxnum > -1 && pq.Count > 0)
+            {
+                var ch = pq.Dequeue();
+                if (nums[ch] == 10)
+                {
+                    nums[ch] = maxnum;
+                    maxnum--;
+                }
+            }
+            int ans = 0;
+            foreach(var s in words)
+            {
+                double tmp = 0;
+                for(int i=s.Length-1; i>=0; i--)
+                {
+                    tmp += nums[s[(s.Length -1) -i]] * Math.Pow(10, i);
+                }
+                ans += (int)tmp;
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ cSharp/greedy/도서관.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.greedy
+{
+    internal class 도서관
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nm[0];
+            int m = nm[1];
+            int[] books = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            List<int> lb = new List<int>() { 0 };
+            List<int> rb = new List<int>() { 0 };
+            foreach(var i in books)
+            {
+                if(i > 0)
+                    rb.Add(i);
+                else
+                    lb.Add(i);
+            }
+            rb.Sort((a,b) => b.CompareTo(a));
+            lb.Sort();
+            int rs = 0;
+            int ls = 0;
+            for(int i=0; i<rb.Count; i+= m)
+            {
+                rs += rb[i] * 2;
+            }
+            for(int i=0; i<lb.Count; i+= m)
+            {
+                ls += -lb[i] * 2;
+            }
+            int maxb = Math.Max(rb.First(), -lb.First());
+            Console.WriteLine(rs + ls - maxb);
+
+        }
+    }
+}
