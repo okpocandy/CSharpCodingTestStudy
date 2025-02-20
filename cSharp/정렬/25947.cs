8985977abcba119889e0dcaf38b3d6c317989195
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-02-21 01:32:37 +0900

    백준 25947, 26091 추가
    백준 25947, 26091 추가

---
+++ cSharp/정렬/25947.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _25947
+    {
+        static void Main(string[] args)
+        {
+            int[] nba = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nba[0]; 
+            int b = nba[1];
+            int a = nba[2];
+            int[] gifts = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(gifts);
+            long[] sum = new long[n];
+            sum[0] = gifts[0];
+            int ans = 0;
+            for(int i=1; i<gifts.Length; i++)
+            {
+                sum[i] = gifts[i] + sum[i-1];
+            }
+            for(int i=0; i<n; i++)
+            {
+                long tmp = 0;
+                int index = (i - a);
+                if(index < 0)
+                {
+                    tmp += sum[i];
+                    tmp /= 2;
+                }
+                else
+                {
+                    tmp += sum[i] - (sum[i] - sum[index]) / 2;
+                }
+                if(tmp <= b)
+                {
+                    ans = i+1;
+                }
+                    
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ cSharp/정렬/26091.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _26091
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nm[0];
+            int m = nm[1];
+            int[] teams = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(teams);
+            int l = 0;
+            int r = n - 1;
+            int ans = 0;
+            while(l < r)
+            {
+                int tmp = teams[l] + teams[r];
+                if(tmp >= m)
+                {
+                    l++;
+                    r--;
+                    ans++;
+                }
+                else
+                {
+                    l++;
+                }
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
