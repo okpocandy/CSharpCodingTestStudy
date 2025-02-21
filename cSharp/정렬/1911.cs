8b5b906ebe097c7c5c4714fb4b8c30e6979b204b
okpocandy <71319122+okpocandy@users.noreply.github.com>
2025-02-22 01:29:12 +0900

    백준 1911, 2170 추가
    백준 1911, 2170 추가

---
+++ cSharp/정렬/1911.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _1911
+    {
+        static void Main(string[] args)
+        {
+            int[] nl = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nl[0];
+            int l = nl[1];
+            List<(int s, int e)> ls = new List<(int s, int e)>();
+            for(int i=0; i<n; i++)
+            {
+                int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                ls.Add((ints[0], ints[1]));
+            }
+            ls.Sort((a,b) => a.s.CompareTo(b.s));
+            int ans = 0;
+            int pre = ls[0].s;
+            foreach(var tmp in ls)
+            {
+                int ms = tmp.s;
+                if (ms > pre)
+                    pre = ms;
+                while(pre < tmp.e)
+                {
+                    pre += l;
+                    ans++;
+                }
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ cSharp/정렬/2170.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.cSharp.정렬
+{
+    internal class _2170
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<(int x, int y)> ls = new List<(int x, int y)>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] xy = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                ls.Add((xy[0], xy[1]));
+            }
+            ls.Sort((a,b) => a.x.CompareTo(b.x));
+
+            int ans = 0;
+            int pre = ls[0].x;
+            foreach(var l  in ls)
+            {
+                int sx = l.x;
+                int sy = l.y;
+                if(pre>= sy)
+                    continue;
+                if (pre <= sx)
+                    pre = sx;
+                ans += sy - pre;
+                pre = sy;
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
